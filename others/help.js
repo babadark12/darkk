5b084f4c1bfb118f16b6f2b5039abe89d0724f11
Mr / Robot <83903153+imr0b0t@users.noreply.github.com>
2021-07-13 01:39:18 +0300

    Update help.js

--- others/help.js
+++ others/help.js
@@ -40,6 +40,7 @@ module.exports = {
 > \`${prefix}emoji\`-\`${prefix}se\`
 > \`${prefix}bans\`-\`${prefix}embed\`
 > \`${prefix}addrole\`-\`${prefix}rmvrole\`
+> \`${prefix}suggest\`-\`${prefix}vkick\`
 > 
 > **Fun Commands**
 > \`${prefix}slap\`-\`${prefix}hug\`
