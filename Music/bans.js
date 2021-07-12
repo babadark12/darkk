d19fbd8d45d8a0f231c4cfae88de44b07ae87cf8
Mr / Robot <83903153+imr0b0t@users.noreply.github.com>
2021-07-12 22:40:03 +0300

    Create bans.js

---
+++ Music/bans.js
@@ -0,0 +1,22 @@
+const { MessageEmbed } = require('discord.js');
+
+module.exports = {
+    name: 'bans',
+    category: "Moderation Commands",
+    description: "List all Server Bans",
+    usage: "bans",
+async execute(message, args) {
+    
+
+        message.guild.fetchBans().then(bans => {
+            let bansembed = new MessageEmbed()
+                .setTitle(`Total Bans`)
+                .setColor("Red")
+                .setDescription(`**Banned User**: ${bans.size}`)
+                .setFooter("")
+                .setTimestamp()
+            message.channel.send(bansembed)
+
+        })
+    }
+}
