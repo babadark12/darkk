fd20e870c065ecf10c78ac15f47664d75a2a8b43
Mr / Robot <83903153+imr0b0t@users.noreply.github.com>
2021-07-12 22:29:54 +0300

    Create se.js

---
+++ Music/se.js
@@ -0,0 +1,35 @@
+const Discord = require("discord.js");
+const { MessageEmbed } = require("discord.js");
+
+module.exports = {
+  name: "se",
+  category: "fun",
+  description: "```Converting Server emoji to PNG/GIF!```",
+  async execute(message, args) {
+
+    const emoji = args[0];
+    if (!emoji) return message.channel.send(`Please Give Me A Emoji!`);
+
+    let customemoji = Discord.Util.parseEmoji(emoji);
+
+    if (customemoji.id) {
+      const Link = `https://cdn.discordapp.com/emojis/${customemoji.id}.${
+        customemoji.animated ? "gif" : "png"
+      }`;
+      
+      const Added = new MessageEmbed()
+        .setTitle("Emoji Converter")
+        .setColor("RANDOM")
+        .addField('Link', `[Click Me](${Link})`)
+        .setImage(Link);
+      return message.channel.send(Added);
+    } else {
+      let CheckEmoji = (emoji, { assetType: "png" });
+      if (!CheckEmoji[0])
+        return message.channel.send(`Please Give Me A Valid Emoji!`);
+      message.channel.send(
+        `You Can Use Normal Emoji Without Adding In Server!`
+      );
+    }
+  }
+};
