6c635befaea3b6652852546abf81e2c60479212f
Mr / Robot <83903153+imr0b0t@users.noreply.github.com>
2021-07-13 00:37:28 +0300

    Update index.js

--- index.js
+++ index.js
@@ -125,35 +125,6 @@ if (!message.member.hasPermission("MANAGE_GUILD")) {
   }
 }) 
 /////
-client.on('message', message => {
-if (message.content.startsWith(prefix + 'list emoji')) {
-  let Emojis = "";
-    let EmojisAnimated = "";
-    let EmojiCount = 0;
-    let Animated = 0;
-    let OverallEmojis = 0;
-    function Emoji(id) {
-      return client.emojis.cache.get(id).toString();
-    }
-    message.guild.emojis.cache.forEach((emoji) => {
-      OverallEmojis++;
-      if (emoji.animated) {
-        Animated++;
-        EmojisAnimated += Emoji(emoji.id);
-      } else {
-        EmojiCount++;
-        Emojis += Emoji(emoji.id);
-      }
-    });
-    let Embed = new Discord.MessageEmbed()
-      .setTitle(`Emojis in ${message.guild.name}.`)
-      .setDescription(`
-        Animated [${Animated}]:\n${EmojisAnimated}\n\nStandard [${EmojiCount}]:\n${Emojis}\n\nAll Emoji [${OverallEmojis}]
-      `)
-      .setColor(RANDOM);
-    message.channel.send(Embed);
-  
-}})
 
 //command Handler DO NOT TOUCH
  const prefixRegex = new RegExp(`^(<@!?${client.user.id}>|${escapeRegex(prefix)})\\s*`);
