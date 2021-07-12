01c7d67f43c1fe997b220d83810aaf5ef9a14197
Mr / Robot <83903153+imr0b0t@users.noreply.github.com>
2021-07-12 22:26:26 +0300

    Create emoji.js

---
+++ Music/emoji.js
@@ -0,0 +1,45 @@
+const { MessageEmbed } = require("discord.js");
+
+//By Legendary Keker
+
+module.exports = {
+    name: "emoji",
+   cooldown: 7,
+    aliases: ["emojiinfo", "info"],
+    description: "Show Emoji Information!",
+    usage: "emoji <emoji>",
+async  execute(message, args) {
+    
+
+        //Start
+
+        if (!args[0] || !args[0].startsWith("<") || !args[0].endsWith(">") || !args[0].includes(":")) return message.channel.send(`Please Give A Valid Custom Emoji!`);
+
+        let Thinger = args[0].split(":");
+
+        let Animated;
+        if (Thinger[0] === "<a") {
+          Animated = true;
+        } else {
+          Animated = false;
+        };
+
+        const Name = Thinger[1];
+        const ID = Thinger[2].slice(0, -1);
+        const Link = `https://cdn.discordapp.com/emojis/${ID}.${Animated ? "gif" : "png"}?v=1`;
+
+        const Embed = new MessageEmbed()
+        .setColor(`RANDOM`)
+        .setThumbnail(Link)
+        .setTitle(`Emoji Information!`)
+        .addField(`Name :`, Name, true)
+        .addField(`ID :`, ID, true)
+        .addField(`Animated :`, Animated ? "Yes" : "No", true)
+        .addField(`Link :`, `[Click Me](${Link})`)
+
+        return message.channel.send(Embed);
+
+        //End
+
+    }
+};
