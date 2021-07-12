2c1301359c540382a5b6b5d54c5f9569baf1b822
Mr / Robot <83903153+imr0b0t@users.noreply.github.com>
2021-07-12 22:25:36 +0300

    Create count.js

---
+++ Music/count.js
@@ -0,0 +1,38 @@
+const { MessageEmbed } = require("discord.js");
+
+module.exports = {
+  name: "membercount",
+  aliases: ["count", "infocount"],
+  category: "count",
+  async execute(message, args) {
+    const Members = message.guild.memberCount;
+    const bots = message.guild.members.cache.filter(
+      member => member.bot === true
+    ).size;
+    const humans = message.guild.members.cache.filter(
+      member => !member.user.bot
+    ).size;
+    const online = message.guild.members.cache.filter(
+      member => member.presence.status === "online"
+    ).size;
+    const offline = message.guild.members.cache.filter(
+      member => member.presence.status === "offline"
+    ).size;
+    const dnd = message.guild.members.cache.filter(
+      member => member.presence.status === "dnd"
+    ).size;
+    const idle = message.guild.members.cache.filter(
+      member => member.presence.status === "idle"
+    ).size;
+
+    const embed = new MessageEmbed()
+      .setColor(`Blue`)
+      .setTitle(`Members Information`)
+      .addField(`**All Members**`, Members)
+      .setThumbnail(message.guild.iconURL({ dynamic: true }))
+      .setAuthor(`${message.guild.name}`)
+      .setTimestamp();
+
+    message.channel.send(embed);
+  }
+};
