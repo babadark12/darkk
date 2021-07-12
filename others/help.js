e00a1e81a4a3c94e3da495193103dec62d8c412c
Mr / Robot <83903153+imr0b0t@users.noreply.github.com>
2021-07-12 22:24:05 +0300

    Update help.js

--- others/help.js
+++ others/help.js
@@ -12,24 +12,22 @@ module.exports = {
     let helpEmbed = new MessageEmbed()
     .setFooter(message.author.username, message.author.displayAvatarURL)
     .setColor('RANDOM')
-    .setTitle("__Help__")
-  .setImage( "https://cdn.discordapp.com/attachments/792368491877498890/792512314620116992/image0.gif")
-  .setDescription(`**[  SUPPORT  ](https://discord.gg/BgFvGtX5p8)**   - [   INVITE   ](https://discord.com/api/oauth2/authorize?client_id=792369278103453709&permissions=3209984&scope=bot)
+    .setTitle("Invite Me")
+    .setURL("https://discord.com/api/oauth2/authorize?client_id=864203179947458571&permissions=8&scope=bot")
+    .setImage("")
+    .setDescription(`
+| Other Commands
+\`${prefix}help\`-\`${prefix}ping\` 
+\`${prefix}prefix\`-\`${prefix}uptime\`
 
-prefix=\`${prefix}\`
-
-Others
-
-\`${prefix}help\`      \`${prefix}ping\` 
-\`${prefix}prefix\`    \`${prefix}uptime\`
-
-Music
+| Music Commands
 
 \`${prefix}filter(fi)\`-\`${prefix}loop (l)\`-\`${prefix}lyrics (ly)\`-\`${prefix}np\`
 \`${prefix}pause\`-\`${prefix}play(p)\`-\`${prefix}queue(qu)\`-\`${prefix}radio\`-\`${prefix}remove\`
 \`${prefix}resume (r)\`-\`${prefix}search\`-\`${prefix}shuffle(mix)\`-\`${prefix}skip(s)\`-\`${prefix}skipto(st)\`-\`${prefix}stop\`
 \`${prefix}volume(vol)\`
 
+| Moderation Commands
 
 `)
     
