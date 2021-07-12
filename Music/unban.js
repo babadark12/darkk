bcc71abd06fdff01bee14b368ade16f6c7c7ca9e
Mr / Robot <83903153+imr0b0t@users.noreply.github.com>
2021-07-12 22:31:32 +0300

    Create unban.js

---
+++ Music/unban.js
@@ -0,0 +1,48 @@
+const { MessageEmbed } = require('discord.js');
+const { denyemoji , aproveemoji , erroremoji } = require("../config.json");
+module.exports = {
+    name: 'unban',
+    async execute(message, args) {
+        if (!message.member.hasPermission('BAN_MEMBERS')) return message.channel.send('You are missing **BAN_MEMBERS** permission!').then(m => m.delete({ timeout: 5000 }));
+
+        if (!args[0]) return message.channel.send('please enter a users id to unban!').then(m => m.delete({ timeout: 5000 }));
+
+        let member;
+
+        try {
+            member = await client.users.fetch(args[0])
+        } catch (e) {
+            console.log(e)
+            return message.channel.send('Not a valid user!').then(m => m.delete({ timeout: 5000 }));
+        }
+
+        const reason = args[1] ? args.slice(1).join(' ') : 'no reason';
+
+        const embed = new MessageEmbed()
+            .setFooter(`${message.author.tag} | ${message.author.id}`, message.author.displayAvatarURL({ dynamic: true }));
+
+        message.guild.fetchBans().then( bans => {
+
+            const user = bans.find(ban => ban.user.id === member.id );
+
+            if (user) {
+                embed.setTitle(`Successfully Unbanned ${user.user.tag}`)
+                    .setColor('#FFF712')
+                    .addField('User ID', user.user.id, true)
+                    .addField('user Tag', user.user.tag, true)
+                    .addField('Banned Reason', user.reason != null ? user.reason : 'no reason')
+                    .addField('Unbanned Reason', reason)
+                message.guild.members.unban(user.user.id, reason).then(() => message.channel.send(embed))
+            } else {
+                embed.setTitle(`User ${member.tag} isn't banned!`)
+                    .setColor('Red')
+                message.channel.send(embed)
+            }
+
+        }).catch(e => {
+            console.log(e)
+            message.channel.send('An error has occurred!')
+        });
+
+    }
+}
