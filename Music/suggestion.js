61795281c25c0243981b7b454f50026f4b0b9b34
Mr / Robot <83903153+imr0b0t@users.noreply.github.com>
2021-07-13 01:31:46 +0300

    Update suggestion.js

--- Music/suggestion.js
+++ Music/suggestion.js
@@ -10,10 +10,10 @@ module.exports = {
       return message.channel.send("Please Give the Suggestion");
     }
 
-    let channel = message.guild.channels.cache.find(x => x.name === "suggestion" || x.name === "suggestions");
+    let channel = message.guild.channels.cache.find(x => x.name === "🔏⎮suggestion" || x.name === "🔏⎮suggestions);
 
     if (!channel) {
-      return message.channel.send("there is no channel with name - suggestions");
+      return message.channel.send("there is no channel with name - 🔏⎮suggestions");
     }
 
     let embed = new MessageEmbed()
