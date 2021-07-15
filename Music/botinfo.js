b96722058d2df87b16d7a6ff4a5e4d61fadd0d4e
Mr / Robot <83903153+imr0b0t@users.noreply.github.com>
2021-07-15 09:52:42 +0300

    Update botinfo.js

--- Music/botinfo.js
+++ Music/botinfo.js
@@ -8,7 +8,7 @@ module.exports = {
     let embed = new discord.MessageEmbed()
       .setTitle(`__**INFORMATION ABOUT BOT**__`)
       .addField("Bot Name", `<@${client.user.id}>`)
-      .addField("Bot Developer", `@Ragnar{}#6067`)
+      .addField("Bot Developer", `@MrRobot#6067`)
       .addField("Total Guilds", `${client.guilds.cache.size}`, true)
       .addField("Total Channels", `${client.channels.cache.size}`)
       .addField("Total Users", `${client.users.cache.size}`, true)
