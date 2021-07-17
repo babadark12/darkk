4c6bde243d19830daabca3a8d757e6426700b14e
Mr / Robot <83903153+imr0b0t@users.noreply.github.com>
2021-07-17 23:29:47 +0300

    Update play.js

--- include/play.js
+++ include/play.js
@@ -138,10 +138,10 @@ module.exports = {
         .setColor("#BLUE")
         .setThumbnail(`https://cdn.discordapp.com/avatars/844069682545164289/b40e8a61cbeb7a6319b5050238a9aa03.png?size=1024`)
         .setImage(thumb)
-        .setDescription(`◈ ━━━━━━━ :-<a:emoji_2:866050145459241010> ━━━━━━━ ◈`)
-        .addField(":-<a:emoji_6:866052139855380531> Requested by:", `\`${message.author.username}#${message.author.discriminator}\``, true)
-        .addField("<a:2play_16:864932819952468038> Length:", `\`${song.duration} Minutes\``, true)
-        .addField("<a:partner_13:864932636635430963> Volume:", `\`100\``, true)
+        .setDescription(`◈ ━━━━━━━ <a:emoji_2:866050145459241010> ━━━━━━━ ◈`)
+        .addField("<a:emoji_6:866052139855380531> Requested by:", `\`${message.author.username}#${message.author.discriminator}\``, true)
+        .addField("Length:", `\`${song.duration} Minutes\``, true)
+        .addField("<a:emoji_9:866052195157409834> Volume:", `\`100\``, true)
 
       var playingMessage = await queue.textChannel.send(newsong);
       
