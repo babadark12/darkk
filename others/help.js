ae820b01ec64ecd3ef66c4b94e8abb2c51f80b5e
Mr / Robot <83903153+imr0b0t@users.noreply.github.com>
2021-07-18 17:12:19 +0300

    Update help.js

--- others/help.js
+++ others/help.js
@@ -12,7 +12,7 @@ module.exports = {
     .setColor('RANDOM')
     .addField("Other", `\`help\`, \`ping\`, \`prefix\`, \`uptime\`, \`avatar\`, \`status\`, \`invites\`, \`suggest\`, \`userinfo\`, \`serverinfo\`, \`botinfo\``)
     .addField("Music", `\`filter\`, \`loop\`, \`lyrics\`, \`np\`, \`pause\`, \`play\`, \`queue\`, \`radio\`, \`remove\`, \`resume\`, \`search\`, \`shuffle\`, \`skip\`, \`skipto\`, \`stop\`, \`volume\``)
-    .addField("Moderation", `\`lock\`, \`unlock\`, \`hide\`, \`unhide\`, \`ban\`, \`unban\`, \`mute\`, \`unmute\`, \`count\`, \`emojiinfo\`, \`se\`, \`bans\`, \`embed\`, \`addrole\`, \`rmvrole\`, \`vkick\`, \`clear\`, \`emojilist\`, \`kick\`, \`nick\`, \`rolinfo\`, \`warn\`, \`botlist\`, \`cinvite\``)
+    .addField("Moderation", `\`lock\`, \`unlock\`, \`hide\`, \`unhide\`, \`ban\`, \`unban\`, \`mute\`, \`unmute\`, \`count\`, \`emojiinfo\`, \`se\`, \`bans\`, \`embed\`, \`addrole\`, \`rmvrole\`, \`vkick\`, \`clear\`, \`emojilist\`, \`kick\`, \`nick\`, \`rolinfo\`, \`warn\`, \`botlist\`, \`cinvite\`, \`channelinfo\``)
     .addField("Gif & Picture", `\`boy\`, \`girl\`, ‌\`couple\`, \`anime\`, \`emoji\`, \`animal\`, \`picboy\`, \`picgirl\`, \`picanimal\`, \`picemoji\`, \`picbaby\``)
     .addField("Text", `\`textkurdish\`, \`textarabic\`, \`textenglish\`, \`textturkish\`, \`textpersian\``)
     .addField("Fun", `\`slap\`, \`hug\`, \`howgay\`,‌ \`rate\`, \`meme\`, \`cry\`, \`kiss\``)
