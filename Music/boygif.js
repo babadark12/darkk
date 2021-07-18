25e3ccbc46b2ee27464777ab0374f50b49b64547
Mr / Robot <83903153+imr0b0t@users.noreply.github.com>
2021-07-18 20:44:26 +0300

    Update boygif.js

--- Music/boygif.js
+++ Music/boygif.js
@@ -16,7 +16,7 @@ module.exports = {
 
     .setTitle("Boy Gif")
     .setURL(replies[result])
-     .setColor("black")
+     .setColor("#277ecd")
     .setFooter(`${message.author.tag} `, message.author.avatarURL)
     .setImage(replies[result]);
 
