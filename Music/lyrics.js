b27c8d0b7486e6ba9fc183e28b7b1de66f067337
Mr / Robot <83903153+imr0b0t@users.noreply.github.com>
2021-07-18 13:55:50 +0300

    Update lyrics.js

--- Music/lyrics.js
+++ Music/lyrics.js
@@ -20,7 +20,7 @@ async execute(message) {
     //if not in a Guild return
     if(!message.guild) return;
     //react with approve emoji
-    message.react("769665713124016128").catch(console.error);
+    message.react("").catch(console.error);
     //Get the current Queue
     const queue = message.client.queue.get(message.guild.id);
     //If no Queue Error
@@ -48,7 +48,7 @@ async execute(message) {
     }
     //define lyrics Embed
     let lyricsEmbed = new MessageEmbed()
-      .setTitle("<:lyrics:769938447279456296> Lyrics")
+      .setTitle("Lyrics")
       .setDescription(lyrics)
       .setColor("#f300e5")
     //if to long make slice it
