70caa3bbba392d3a6a4eadf0484445d5a60aae54
Mr / Robot <83903153+imr0b0t@users.noreply.github.com>
2021-07-18 09:43:27 +0300

    Create say.js

---
+++ Music/say.js
@@ -0,0 +1,20 @@
+const db = require("quick.db");
+
+module.exports = {
+  name: "say",
+  category: "misc",
+  description: "Get bot ping :/",
+  bot: ["MANAGE_MESSAGES"],
+  usage: "say <msg>",
+  args: true,
+  async execute(message, args, client) {
+
+    message.delete();
+    const usa = args.join(" ");
+    if (!usa) return message.channel.send(`${message.author}, say <msg>`);
+    let say = args.join(" ");
+    // const Channel = member.guild.channels.cache.get('797491226567114753') //insert channel id that you want to send to
+
+    message.channel.send(say);
+  }
+};
