eef70adb8250bbe37fa945e4b305fbfffe664f4a
Mr / Robot <83903153+imr0b0t@users.noreply.github.com>
2021-07-18 12:58:50 +0300

    Create daily.js

---
+++ Music/daily.js
@@ -0,0 +1,38 @@
+const Discord = require("discord.js");
+const ms = require("parse-ms");
+const db = require("quick.db");
+
+module.exports = {
+  name: "daily",
+
+  async execute(message, args, client) {
+    let user = message.author;
+
+    let timeout = 86400000;
+    let amount = 1500;
+
+    let daily = await db.fetch(`daily_${user.id}`);
+
+    if (daily !== null && timeout - (Date.now() - daily) > 0) {
+      let time = ms(timeout - (Date.now() - daily));
+
+      let timeEmbed = new Discord.MessageEmbed()
+        .setColor("#FFFFFF")
+        .setDescription(
+          `:x: You've already collected your daily reward\n\nCollect it again in __${time.hours}__h __${time.minutes}__m __${time.seconds}__s `
+        )
+        .setColor("RED");
+      message.reply({
+        embeds: [timeEmbed],
+        allowedMentions: { repliedUser: false },
+      });
+    } else {
+      message.reply({
+        content: `You've collected your daily reward of **${amount}** Coins!`,
+        allowedMentions: { repliedUser: false },
+      });
+      db.add(`money_${user.id}`, amount);
+      db.set(`daily_${user.id}`, Date.now());
+    }
+  },
+};
