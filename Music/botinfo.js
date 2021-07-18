576c462069096ddcd58e1502740ce129d6ede68b
Mr / Robot <83903153+imr0b0t@users.noreply.github.com>
2021-07-18 20:44:09 +0300

    Update botinfo.js

--- Music/botinfo.js
+++ Music/botinfo.js
@@ -8,12 +8,12 @@ module.exports = {
     let embed = new discord.MessageEmbed()
       .setTitle(`__**INFORMATION ABOUT BOT**__`)
       .addField("Bot Name", `<@${client.user.id}>`)
-      .addField("Bot Developer", `@Elliot#6067`)
+      .addField("Bot Developer", `@MrRobot#6067`)
       .addField("Total Guilds", `${client.guilds.cache.size}`, true)
       .addField("Total Channels", `${client.channels.cache.size}`)
       .addField("Total Users", `${client.users.cache.size}`, true)
       .addField("Bot Libary", `discord.js`)
-      .setColor("RANDOM")
+      .setColor("#277ecd")
       .setFooter(`information about bot`);
       message.channel.send(embed);
   }
