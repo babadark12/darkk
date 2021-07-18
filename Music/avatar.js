b0303822d0b77bd23aa1edbdd1141c4a8739c5df
Mr / Robot <83903153+imr0b0t@users.noreply.github.com>
2021-07-18 20:41:40 +0300

    Update avatar.js

--- Music/avatar.js
+++ Music/avatar.js
@@ -10,7 +10,7 @@ module.exports = {
         const member = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(r => r.user.username.toLowerCase() === args.join(' ').toLocaleLowerCase()) || message.guild.members.cache.find(r => r.displayName.toLowerCase() === args.join(' ').toLocaleLowerCase()) || message.member;
     const embed = new Discord.MessageEmbed()
         .setAuthor(member.user.tag, member.user.displayAvatarURL({ dynamic: true, size: 1024 }))
-        .setColor("RANDOM")
+        .setColor("#277ecd")
         .setTitle(`Links`)
         .setDescription(`**[png](${member.user.displayAvatarURL({format: "png", size: 1024})}) | [jpg](${member.user.displayAvatarURL({format: "jpg", size: 1024})}) | [gif](${member.user.displayAvatarURL({format: "gif", size: 1024, dynamic: true})}) | [webp](${member.user.displayAvatarURL({format: "webp", size: 1024})})**`)
         .setImage(member.user.displayAvatarURL({ dynamic: true, size: 1024 }))
