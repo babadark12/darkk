6ea1e59a08fce49154b3da7631fe75a85660a512
Mr / Robot <83903153+imr0b0t@users.noreply.github.com>
2021-07-18 20:47:15 +0300

    Update girlgif.js

--- Music/girlgif.js
+++ Music/girlgif.js
@@ -16,7 +16,7 @@ module.exports = {
 
     .setTitle("Girl Gif")
 
-    .setColor("black")
+    .setColor("#277ecd")
     .setFooter(`${message.author.tag} `, message.author.avatarURL)
     .setImage(replies[result]);
 
