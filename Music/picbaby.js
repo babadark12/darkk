7d682a97771d7314d14c3e033df84a0a9f2b1947
Mr / Robot <83903153+imr0b0t@users.noreply.github.com>
2021-07-18 08:52:44 +0300

    Create picbaby.js

---
+++ Music/picbaby.js
@@ -0,0 +1,27 @@
+const Discord = require("discord.js");
+const { MessageEmbed } = require("discord.js");
+
+module.exports = {
+  name: "picbaby",
+  aliases: [],
+  description: "Show Photo",
+  usage: "Photo",
+  async execute(message, args, client) {
+   
+    let replies = ["https://cdn.discordapp.com/attachments/699339102989844500/854394237868834886/image2.jpg","https://cdn.discordapp.com/attachments/699339102989844500/854394237499998218/image1.jpg","https://cdn.discordapp.com/attachments/699339102989844500/854394237286350858/image0.jpg","https://cdn.discordapp.com/attachments/699339102989844500/854351238082461696/cift_y.png","https://cdn.discordapp.com/attachments/699339102989844500/854351121353932800/cift_h.jpg","https://cdn.discordapp.com/attachments/699339102989844500/853235886040285224/0407a5f6d8627c0585f1ed88692a2beb.png","https://cdn.discordapp.com/attachments/699339102989844500/853014123894145085/IMG_20210608_003506.jpg"];
+
+    let result = Math.floor((Math.random() * replies.length));
+    
+    let photoembed = new Discord.MessageEmbed()
+
+    .setTitle("Baby Photo")
+    
+      .setColor("RANDOM")
+    .setFooter(`${message.author.tag} `, message.author.avatarURL)
+    .setImage(replies[result]);
+
+    message.channel.send(photoembed);
+
+   
+  }
+};
