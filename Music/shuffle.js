07937b9c69a044b48dbda2ffd615a3eba58d8be4
Mr / Robot <83903153+imr0b0t@users.noreply.github.com>
2021-07-18 14:27:24 +0300

    Update shuffle.js

--- Music/shuffle.js
+++ Music/shuffle.js
@@ -9,37 +9,31 @@ const { PREFIX } = require(`../config.json`);
 //////COMMAND BEGIN/////////
 ////////////////////////////
 module.exports = {
-  name: "shuffle",
-  aliases: ["mix"],
-  description: "Shuffles the current Queue!",
+  name: "skip",
+  aliases: ["s"],
+  description: "Skip the currently playing song",
   cooldown: 5,
-  edesc: `Type the Command, if a Queue exists to change to Order of the Queue, randomly.\nUsage: ${PREFIX}shuffle`,
+  edesc: `Type the Command, to skip to current listening song.\nUsage: ${PREFIX}skip`,
 
-execute(message,args,client) {
-    //if not in a guild return
-    if(!message.guild) return;
+execute(message) {
+    //if not in a guild retunr
+    if (!message.guild) return;
     //react with approve emoji
-    message.react("").catch(console.error);
-    //get the Queue
+    message.react("✅").catch(console.error);
+    //get the queue
     const queue = message.client.queue.get(message.guild.id);
-    //if no queue return error
-    if (!queue) return attentionembed(message, "There is no Queue.");
-    //if not in the same channel as the bot
+    //if no Queue return error
+    if (!queue)
+      return attentionembed(message, "There is nothing you can skip!").catch(console.error);
+    //if not in the same channel return
     if (!canModifyQueue(message.member)) return;
-    //get all queue songs
-    let songs = queue.songs;
-    //make an array and mix them
-    for (let i = songs.length - 1; i > 1; i--) {
-      let j = 1 + Math.floor(Math.random() * i);
-      [songs[i], songs[j]] = [songs[j], songs[i]];
-    }
-    //define queue.songs
-    queue.songs = songs;
-    //set the Queue
-    message.client.queue.set(message.guild.id, queue);
-    //send the Approve message
-    queue.textChannel.send(new MessageEmbed()
-    .setDescription(`**✅ | ${message.author} shuffled the Queue**`)
-    .setColor("#c219d8")).catch(console.error);
+    //set playing to true
+    queue.playing = true;
+    //end current song
+    queue.connection.dispatcher.end();
+    //send approve message
+    queue.textChannel.send(
+      new MessageEmbed().setColor("#c219d8").setAuthor(`${message.author.username} skipped the song.`, "https://cdn.discordapp.com/emojis/769915194444480542.png")
+    ).catch(console.error);
   }
 };
