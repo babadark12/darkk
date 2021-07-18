6e01b40900191eb48000e0eee0a175684572b456
Mr / Robot <83903153+imr0b0t@users.noreply.github.com>
2021-07-18 16:22:42 +0300

    Update help.js

--- others/help.js
+++ others/help.js
@@ -10,8 +10,13 @@ module.exports = {
     let helpEmbed = new MessageEmbed()
     .setFooter(message.author.username, message.author.displayAvatarURL)
     .setColor('RANDOM')
-    .addField("Other Commands", `\`help\`, \`ping\`, \`prefix\`, \`uptime\`, \`avatar\`, \`status\`, \`invites\`, \`suggest\`, \`userinfo\`, \`serverinfo\`, \`botinfo\``)
-    .addField("Robot", `Hich`)
+    .addField("Other", `\`help\`, \`ping\`, \`prefix\`, \`uptime\`, \`avatar\`, \`status\`, \`invites\`, \`suggest\`, \`userinfo\`, \`serverinfo\`, \`botinfo\``)
+    .addField("Music", `\`filter\`, \`loop\`, \`lyrics\`, \`np\`, \`pause\`, \`play\`, \`queue\`, \`radio\`, \`remove\`, \`resume\`, \`search\`, \`shuffle\`, \`skip\`, \`skipto\`, \`stop\`, \`volume\``)
+    .addField("Moderation", `\`lock\`, \`unlock\`, \`hide\`, \`unhide\`, \`ban\`, \`unban\`, \`mute\`, \`unmute\`, \`count\`, \`emojiinfo\`, \`se\`, \`bans\`, \`embed\`, \`addrole\`, \`rmvrole\`, \`vkick\`, \`clear\`, \`emojilist\`, \`kick\`, \`nick\`, \`rolinfo\`, \`warn\`, \`botlist\`, \`cinvite\``)
+    .addField("Gif & Picture", `\`boy\`, \`girl\`, ‌\`couple\`, \`anime\`, \`emoji\`, \`animal\`, \`picboy\`, \`picgirl\`, \`picanimal\`, \`picemoji\`, \`picbaby\``)
+    .addField("Text", `\`textkurdish\`, \`textarabic\`, \`textenglish\`, \`textturkish\`, \`textpersian\``)
+    .addField("Fun", `\`slap\`, \`hug\`, \`howgay\`,‌ \`rate\`, \`meme\`, \`cry\`, \`kiss\``)
+    .addField("Backup", `\`backup-create\`, \`backup-load\`, \`backup-info\``)
     helpEmbed.setTimestamp();
 
     return message.channel.send(helpEmbed).catch(console.error);
