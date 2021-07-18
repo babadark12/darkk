d6244d8ee30fa1469e6345f9fe2b00a7230872d0
Mr / Robot <83903153+imr0b0t@users.noreply.github.com>
2021-07-18 09:01:46 +0300

    Update couplegif.js

--- Music/couplegif.js
+++ Music/couplegif.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const { MessageEmbed } = require("discord.js");
 
 module.exports = {
-  name: "couple",
+  name: "gifcouple",
   aliases: ["Couple"],
   description: "Show Gif",
   usage: "Gif",
