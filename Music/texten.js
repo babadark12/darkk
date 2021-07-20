2531d4f102cc8c19621c6f36c926e9a705425a52
Mr / Robot <83903153+imr0b0t@users.noreply.github.com>
2021-07-21 00:30:38 +0300

    Update texten.js

--- Music/texten.js
+++ Music/texten.js
@@ -19,8 +19,8 @@ module.exports = {
     let result = Math.floor((Math.random() * replies.length));
 
     message.channel.send(replies[result]).then(message => {
-			message.react('<a:emoji_17:866295572424163368>').then(r => {
-				message.react('<a:emoji_16:866295550469865483>');
+			message.react('<a:emoji_21:867156106464067624>').then(r => {
+				message.react('');
            });
           });
       }
