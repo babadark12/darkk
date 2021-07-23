4ccb0725691b49734e6550de3017d86dd9ca6c04
/MrRobot <87871568+kakr0b0t@users.noreply.github.com>
2021-07-24 00:09:00 +0300

    Create lock.js

---
+++ Music/lock.js
@@ -0,0 +1,22 @@
+const Discord = module.require("discord.js");
+
+module.exports = {
+   name: "lock",
+   description: "lock a channel",
+    usage: "lock",
+  category: "admin",
+    permissions: "MANAGE_CHANNELS",
+    bot: ["MANAGE_CHANNELS"],
+   async execute(message, args, client) {
+   if (!message.member.hasPermission('MANAGE_SERVER', 'MANAGE_CHANNELS')) {
+   return message.channel.send("You don't have enough Permissions")
+   }
+   message.channel.overwritePermissions([
+     {
+        id: message.guild.id,
+        deny : ['SEND_MESSAGES'],
+     },
+    ],);
+   message.channel.send(`**🔓 - ${message.channel}  has been locked**`)
+}
+}
