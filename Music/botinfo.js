ea6120126f7af116e3b7a9b114eebfb47838018d
/MrRobot <87871568+kakr0b0t@users.noreply.github.com>
2021-07-24 08:45:17 +0300

    Update botinfo.js

--- Music/botinfo.js
+++ Music/botinfo.js
@@ -7,11 +7,11 @@ module.exports = {
   async execute(message, args, client) {
     let embed = new discord.MessageEmbed()
       .setTitle(`__**INFORMATION ABOUT BOT**__`)
-      .addField("Bot Name", `<@${client.user.id}>`)
+      .addField("Bot Name", `<@${bot.user.id}>`)
       .addField("Bot Developer", `@MrRobot#7265`)
-      .addField("Total Guilds", `${client.guilds.cache.size}`, true)
-      .addField("Total Channels", `${client.channels.cache.size}`)
-      .addField("Total Users", `${client.users.cache.size}`, true)
+      .addField("Total Guilds", `${bot.guilds.cache.size}`, true)
+      .addField("Total Channels", `${bot.channels.cache.size}`)
+      .addField("Total Users", `${bot.users.cache.size}`, true)
       .addField("Bot Libary", `discord.js`)
       .setColor("#277ecd")
       .setFooter(`information about bot`);
