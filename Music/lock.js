91f5b165c2ab14ea34b34556479402272f46f164
/MrRobot <87871568+kakr0b0t@users.noreply.github.com>
2021-07-24 20:46:23 +0300

    Update lock.js

--- Music/lock.js
+++ Music/lock.js
@@ -1,22 +1,36 @@
-const Discord = module.require("discord.js");
-
+const { MessageEmbed } = require("discord.js");
+const Discord = require("discord.js")
+const { lineReply } = require("discord-reply")
+const db = require('quick.db');
+const { MessageButton , MessageActionRow } = require
+ ("discord-buttons")
+ 
 module.exports = {
-   name: "lock",
-   description: "lock a channel",
-    usage: "lock",
-  category: "admin",
-    permissions: "MANAGE_CHANNELS",
-    bot: ["MANAGE_CHANNELS"],
-   async execute(message, args, client) {
-   if (!message.member.hasPermission('MANAGE_SERVER', 'MANAGE_CHANNELS')) {
-   return message.channel.send("You don't have enough Permissions")
-   }
-   message.channel.overwritePermissions([
-     {
-        id: message.guild.id,
-        deny : ['SEND_MESSAGES'],
-     },
-    ],);
-   message.channel.send(`**🔓 ${message.channel}  has been locked.**`)
-}
-}
+    name: "lock",
+    cooldown: 5,
+    aliases: ["lock"],
+    usage: "lock [#channel]",
+    category: "admin",
+    description : "make everyone can't send messages is chat",
+    async execute(message, args, client) {
+
+if(!message.member.hasPermission("MANAGE_CHANNELS")) return message.lineReplyNoMention(
+new MessageEmbed()
+.setColor("RED")
+.setDescription("**You Need `MANAGE_CHANNELS` Permission To Use This Command!**")
+.setFooter(`${message.author.tag}`, message.author.avatarURL()))
+        let channel = message.mentions.channels.first();
+        let channel_find = message.guild.channels.cache.find(ch => ch.id == channel);
+        if (!channel) channel_find = message.channel
+        if (!channel_find) return;
+        channel_find.updateOverwrite(message.guild.id, {
+            SEND_MESSAGES: false
+        });
+        message.lineReplyNoMention(new MessageEmbed()
+	    .setColor("RED")
+	    .setDescription("**:lock: ${message.channel} has been locked.**")
+	    .setFooter(`${message.author.tag}`, message.author.avatarURL()))
+ 
+	
+    }
+};
