6f6a73383fe42c919a92f0b1bd75a603c5a8c76c
/MrRobot <87871568+kakr0b0t@users.noreply.github.com>
2021-07-24 11:44:12 +0300

    Create botinfo.js

---
+++ Music/botinfo.js
@@ -0,0 +1,20 @@
+const discord = require("discord.js");
+
+module.exports = {
+  name: "botinfo",
+  category: "info",
+  description: "INVITE BOT",
+  async execute(message, args, client) {
+    let embed = new discord.MessageEmbed()
+      .setTitle(`__**INFORMATION ABOUT BOT**__`)
+      .addField("Bot Name", `<@${client.user.id}>`)
+      .addField("Bot Developer", `@MrRobot#7265`)
+      .addField("Total Guilds", `${client.guilds.cache.size}`, true)
+      .addField("Total Channels", `${client.channels.cache.size}`)
+      .addField("Total Users", `${client.guilds.cache.reduce((a, g) => a + g.memberCount, 0)}`, true)
+      .addField("Bot Libary", `discord.js`)
+      .setColor("#277ecd")
+      .setFooter(`information about bot`);
+      message.channel.send(embed);
+  }
+};
