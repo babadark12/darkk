201d5e432e602d2a85e1e121f6ecae98e26d7ff4
/MrRobot <87871568+kakr0b0t@users.noreply.github.com>
2021-07-25 12:42:52 +0300

    Create neon.js

---
+++ Music/neon.js
@@ -0,0 +1,45 @@
+const Discord = require("discord.js");
+const { MessageEmbed } = require("discord.js");
+
+module.exports = {
+  name: "neon",
+  aliases: ["neongif", "gifneon"],
+  description: "Show Photo",
+  usage: "Photo",
+  async execute(message, args, client) {
+   
+    let replies = [
+      "https://media.discordapp.net/attachments/755890505681731714/856207944953233439/a_08ca790f778a7ee65184020eb2c85124.gif",
+      "https://media.discordapp.net/attachments/755890505681731714/856212748844007454/image4.gif",
+      "https://media.discordapp.net/attachments/755890505681731714/856203154143313970/hhhh.gif",
+      "https://media.discordapp.net/attachments/755890505681731714/856200984888082472/Comp_105.gif",
+      "https://media.discordapp.net/attachments/755890505681731714/856145244059729920/uurekzneon_17.gif",
+      "https://media.discordapp.net/attachments/755890505681731714/856138999771693076/a_0163926dade85d15a4c7e9f90bd9905c.gif",
+      "https://media.discordapp.net/attachments/755890505681731714/856137212385951744/image0.gif",
+      "https://media.discordapp.net/attachments/755890505681731714/856134635972591636/Comp_22.gif",
+      "https://media.discordapp.net/attachments/755890505681731714/856094705813422090/a_a46b7692563509a4a7332280f8aed01f-1.gif",
+      "https://media.discordapp.net/attachments/755890505681731714/856066337788133377/neon_1-1.gif",
+      "https://media.discordapp.net/attachments/755890505681731714/856039171788767292/sol._of.gif",
+      "https://media.discordapp.net/attachments/755890505681731714/856034861047480320/Nice_gif_pfp.gif",
+      "https://media.discordapp.net/attachments/755890505681731714/855995485688954900/a_4f85b11851dd2fd40c275af63e24ca9c.gif",
+      "https://media.discordapp.net/attachments/755890505681731714/855901403768029255/Gif8.gif",
+      "https://media.discordapp.net/attachments/755890505681731714/855894776896159804/a_622822eb42bf3c49c83f342290a304b6.gif",
+      "https://media.discordapp.net/attachments/755890505681731714/855841642979786752/826380519012565012.gif",
+      "https://media.discordapp.net/attachments/755890505681731714/855788419716218890/image0.gif"
+    ];
+
+    let result = Math.floor((Math.random() * replies.length));
+    
+    let photoembed = new Discord.MessageEmbed()
+
+    .setTitle("Neon Gif")
+    .setURL(replies[result])
+    .setColor("RANDOM")
+    .setFooter(`${message.author.tag} `, message.author.avatarURL)
+    .setImage(replies[result]);
+
+    message.channel.send(photoembed);
+
+   
+  }
+};
