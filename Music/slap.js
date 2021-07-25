04776fb7a2159f0f003b1d70f1c232e1a039f034
/MrRobot <87871568+kakr0b0t@users.noreply.github.com>
2021-07-25 17:20:08 +0300

    Update slap.js

--- Music/slap.js
+++ Music/slap.js
@@ -14,7 +14,7 @@ module.exports = {
 
     const embed = new MessageEmbed()
       .setFooter(message.author.username)
-      .setColor("RANDOM")
+      .setColor("#0d2943")
       .setTitle(`${message.author.username} Slapped ${slapped}`)
       .setDescription(`[Click here if the image failed to load.](${data.url})`)
       .setImage(`${data.url}`)
