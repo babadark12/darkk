b304680569af63a04bfafd01d8bf4038c52a4e35
/MrRobot <87871568+kakr0b0t@users.noreply.github.com>
2021-07-25 16:19:34 +0300

    Update guildinfo.js

--- Music/guildinfo.js
+++ Music/guildinfo.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const { MessageEmbed } = require("discord.js");
 
 module.exports = {
-  name: "server",
+  name: "serverinfo",
   aliases: [],
   description: "Show Server Information!",
   usage: "Serverinfo",
