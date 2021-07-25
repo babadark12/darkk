92fccaa30362b4cd9fa766a92c6b68177b5c9698
/MrRobot <87871568+kakr0b0t@users.noreply.github.com>
2021-07-26 00:29:40 +0300

    Update comment.js

--- Music/comment.js
+++ Music/comment.js
@@ -6,14 +6,14 @@ module.exports = {
   category: "fun",
   async execute(message, args, client) {
   	const comment = args.join('');
-        if(!comment) return message.channel.send(`${client.emotes.error} Provide something to Comment!`)
+        if(!comment) return message.channel.send(`🚫 Provide something to Comment!`)
         try {    
         let yt = await canvacord.Canvas.youtube({"avatar":message.author.displayAvatarURL({format: "png"}),"username":message.author.username, "content":args.join(" ")})
         let attachment = new client.discord.MessageAttachment(yt, 'comment.png')
         message.channel.send(attachment)
     }catch(err) {
         const embed2 = new Discord.MessageEmbed()
-    .setTitle(`${client.emotes.error} Something went wrong.\n${client.emotes.error}Note : It won't work if the User contains Unwanted characters in his Username.`)
+    .setTitle(`🚫 Something went wrong.\n🚫Note : It won't work if the User contains Unwanted characters in his Username.`)
     .setColor("RANDOM")
     message.channel.send(embed2)
     }
