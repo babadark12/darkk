afe46b21c06fbdd49fe29f757449740fba2fc1f0
/MrRobot <87871568+kakr0b0t@users.noreply.github.com>
2021-07-25 12:49:15 +0300

    Update bans.js

--- Music/bans.js
+++ Music/bans.js
@@ -1,22 +1,28 @@
-const { MessageEmbed } = require('discord.js');
+const { MessageEmbed } = require("discord.js");
+const db = require('quick.db');
+const { MessageButton , MessageActionRow } = require(`discord-buttons`)
+const { lineReply } = require("discord-reply");
+
 
 module.exports = {
-    name: 'bans',
-    category: "Moderation Commands",
-    description: "List all Server Bans",
+    name: "bans",
+    cooldown: 5,
+    aliases: ["bans"],
     usage: "bans",
-async execute(message, args) {
-    
-
-        message.guild.fetchBans().then(bans => {
-            let bansembed = new MessageEmbed()
-                .setTitle(`Total Bans`)
-                .setColor("#277ecd")
-                .setDescription(`**Banned User**: ${bans.size}`)
-                .setFooter("")
-                .setTimestamp()
-            message.channel.send(bansembed)
-
-        })
+    category: "admin",
+    description : "To show all server bans",
+    async execute(message, args, client) {
+	if(!message.member.hasPermission("ADMINISTRATOR")) return message.lineReplyNoMention(new MessageEmbed()
+	.setColor("RED")
+	.setDescription(`**You Need** \`ADMINISTRATOR\` **Permission To Use This Command!**`)
+	.setFooter(`${message.author.tag}`, message.author.avatarURL()))
+if (!message.channel.guild) return;
+		message.channel;
+		message.guild.fetchBans().then(bans =>
+message.lineReplyNoMention (new MessageEmbed()
+.setAuthor(message.guild.name, message.guild.iconURL())
+.setColor("BLUE")
+.setDescription(`**${message.guild.name}** ban list is \`${bans.size}\``)
+.setFooter(`${message.author.tag}`, message.author.avatarURL()))).catch(console.error);
     }
-}
+};
