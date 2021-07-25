b77b5dd0b66ff98493a5de7b05888c7953a47fba
/MrRobot <87871568+kakr0b0t@users.noreply.github.com>
2021-07-25 17:36:48 +0300

    Update roleinfo.js

--- Music/roleinfo.js
+++ Music/roleinfo.js
@@ -1,4 +1,5 @@
-const { MessageEmbed } = require("discord.js");
+const discord = require("discord.js");
+const { lineReply } = require("discord-reply");
 
 module.exports = {
         name: 'roleinfo',
@@ -28,11 +29,11 @@ module.exports = {
             .addField("**Color**", role.color, true)
             .addField("**Members**", role.members.size, true)
             .addField("**Position**", role.position, true)
-  //          .addField("**Permissions**", role.permission.join(", "), true)
+            .addField("**Permissions**", role.permission.join(", "), true)
             .addField("**Mentionable**", status[role.mentionable], true)
             .setFooter(message.member.displayName, message.author.displayAvatarURL(), true)
             .setTimestamp()
 
-        message.channel.send(roleembed);
+        message.lineReplyNoMention(roleembed).catch(console.error);
     }
 }
