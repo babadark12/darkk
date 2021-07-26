210a615266bb7fce869651dda3a8810fd6741029
/MrRobot <87871568+kakr0b0t@users.noreply.github.com>
2021-07-26 15:37:21 +0300

    Update lockall.js

--- Music/lockall.js
+++ Music/lockall.js
@@ -26,7 +26,7 @@ new MessageEmbed()
         channels_find.updateOverwrite(message.guild.id, {
             SEND_MESSAGES: false
         });
-        message.lineReplyNoMention(`**🔒 ${message.channel} has been locked.**`)
+        message.lineReplyNoMention(`**🔒 All LOCKED**`)
  
 	
     }
