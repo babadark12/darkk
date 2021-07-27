04cfe37fde2abc83a9844ef11b85d9d3e7f5dcdc
/MrRobot <87871568+kakr0b0t@users.noreply.github.com>
2021-07-27 14:58:05 +0300

    Create vkickall.js

---
+++ Music/vkickall.js
@@ -0,0 +1,21 @@
+const discord = require("discord.js");
+
+module.exports = {
+  name: "vkick all",
+  aliases: ["vkick all"],
+  description: "Show Gif",
+  usage: "vkick all",
+  async execute(message, args, client) {
+   
+  
+ if (!message.member.hasPermission("UNMUTE_MEMBERS"))
+     {
+      return;
+    }
+  let channel = message.member.voice.channel;
+        for (let member of channel.members) {
+            member[1].voice.setChannel(null)
+        }
+        message.channel.send("Done i have kicked all persons who were in your vc")
+  }
+    }
