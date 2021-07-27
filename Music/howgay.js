4fbcf279b62baaa495c01d9d73b6e1762ca5de7c
/MrRobot <87871568+kakr0b0t@users.noreply.github.com>
2021-07-27 22:14:30 +0300

    Update howgay.js

--- Music/howgay.js
+++ Music/howgay.js
@@ -1,5 +1,6 @@
 const Discord = require("discord.js");
 const { MessageEmbed } = require("discord.js");
+const { lineReply } = require("discord-reply");
 
 module.exports = {
   name: "howgay",
@@ -22,7 +23,7 @@ module.exports = {
       .setFooter(`Requested by ${message.author.username}`)
       .setTimestamp();
 
-    message.channel.send(embed);
+    message.lineReplyNoMention(embed);
 
     
   }
