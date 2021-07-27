141e0a1858db2021f10003e6170cadace80f3bbb
/MrRobot <87871568+kakr0b0t@users.noreply.github.com>
2021-07-27 13:19:28 +0300

    Create set-up.js

---
+++ Music/set-up.js
@@ -0,0 +1,25 @@
+const Discord = require("discord.js");
+const db = require("quick.db");
+
+module.exports = {
+  name: "set-up",
+  aliases: ["set-up"],
+  description: "Show Gif",
+  usage: "Gif",
+  async execute(message, args, client) {
+     if (!message.member.hasPermission("MANAGE_SERVER"))
+     {
+      return;
+    }
+message.guild.channels.create('akeno | request', {
+  type: 'category',
+  permissionOverwrites: [
+     {
+       id: message.guild.roles.everyone.id,
+       allow: ['VIEW_CHANNEL'],
+    },
+  ],
+})
+message.reply("Done setup");
+  }
+}
