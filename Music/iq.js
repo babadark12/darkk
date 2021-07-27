11203728fadc84850c9e667c2039c6bbb7cf80c7
/MrRobot <87871568+kakr0b0t@users.noreply.github.com>
2021-07-27 23:32:46 +0300

    Create iq.js

---
+++ Music/iq.js
@@ -0,0 +1,25 @@
+const Discord = require("discord.js");
+
+module.exports = {
+  name: "iq",
+  aliases: [" iq"],
+  description: "Show Gif",
+  usage: "Gif",
+  async execute(message, args, client) {
+
+const iq = Math.floor(Math.random() * 226);
+const embed = new Discord.MessageEmbed()
+  .setTitle(":brain: IQ Test:")
+  .setDescription(":bulb: " + message.author.username + " IQ: `" + iq + "`")
+  .setColor(`RANDOM`)
+  .setTimestamp()
+message.channel.send(embed);
+
+} catch (err) {
+    message.channel.send({embed: {
+      color: 16734039,
+      description: "Something went wrong... :cry:"
+    }})
+  }
+}
+}
