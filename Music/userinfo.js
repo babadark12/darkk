3ac2513e716bc0b4a7e872a8cc7c8c6b9140c025
/MrRobot <87871568+kakr0b0t@users.noreply.github.com>
2021-07-28 20:30:21 +0300

    Update userinfo.js

--- Music/userinfo.js
+++ Music/userinfo.js
@@ -22,21 +22,19 @@ module.exports = {
     const Embed = new Discord.MessageEmbed()
     .setColor(Color)
     .setAuthor(message.guild.ownerID == User.user.id ? "Owner" : "User", Avatar)
-    .addField("User:", `${User.user.username}#${Dism}`) //1
-    .addField("ID:", `${User.id}`) //2
-    .addField("Bot:", `${Bot}`) //3
-    .addField("Position:", `${Position}`) //4
-    .addField("Manageable:", `${User.manageable ? "Yes" : "No"}`) //5
-    .addField("Number of Roles:", `${Roles}`) //6
-    .addField("Created:", `${Created}`) // 7
-    .addField("Joined:", `${Joined}`) // 8
-    .addField("Status:", `${Status}`) // 9
+    .addField("👤 User:", `${User.user.username}#${Dism}`) //1
+    .addField("🆔 ID:", `${User.id}`) //2
+    .addField("🤖 Bot:", `${Bot}`) //3
+    .addField("🔻 Position:", `${Position}`) //4
+    .addField("🎗️ Manageable:", `${User.manageable ? "Yes" : "No"}`) //5
+    .addField("🗃️ Number of Roles:", `${Roles}`) //6
+    .addField("🧭 Created:", `${Created}`) // 7
+    .addField("⌛ Joined:", `${Joined}`) // 8
 
-    .addField("Avatar URLs", `[webp](${User.user.displayAvatarURL({ dynamic: true, format: 'webp'})}) | [png](${User.user.displayAvatarURL({ dynamic: true, format: 'png'})}) | [jpg](${User.user.displayAvatarURL({ dynamic: true, format: 'jpeg'})}) | [jpeg](${User.user.displayAvatarURL({ dynamic: true, format: 'jpeg'})}) | [gif](${User.user.displayAvatarURL({ dynamic: true, format: 'gif'})})`) //10
-    .addField("Using Discord On:", `${Place ? Place.join("\n") == "" ? "Unknown" : Place.join("\n") : "None"}`) //11
-    .addField("Activities", `${Activity.length > 1 ? "" : "Activity"}\n${Activity ? Activity.join("\n") == "" ? "None" : Activity.join("\n") : "None"}`) //11
-    .addField("Badges", `\n${Badges ? Badges : "No badges"}`) //12
-    .addField("Permissions", `**\`${Permissions ? Permissions : "None"}\`**`) // 13
+    .addField("📎 Avatar URLs", `[webp](${User.user.displayAvatarURL({ dynamic: true, format: 'webp'})}) | [png](${User.user.displayAvatarURL({ dynamic: true, format: 'png'})}) | [jpg](${User.user.displayAvatarURL({ dynamic: true, format: 'jpeg'})}) | [jpeg](${User.user.displayAvatarURL({ dynamic: true, format: 'jpeg'})}) | [gif](${User.user.displayAvatarURL({ dynamic: true, format: 'gif'})})`) //10
+    .addField("➕ Using Discord On:", `${Place ? Place.join("\n") == "" ? "Unknown" : Place.join("\n") : "None"}`) //11
+    .addField("🔮 Activities", `${Activity.length > 1 ? "" : "Activity"}\n${Activity ? Activity.join("\n") == "" ? "None" : Activity.join("\n") : "None"}`) //11
+    .addField("🎴 Badges", `\n${Badges ? Badges : "No badges"}`) //12
     .setThumbnail(User.user.displayAvatarURL({ size: 512, dynamic: true }))
     .setFooter(`Requested by ${message.author.tag}`, message.author.displayAvatarURL({ dynamic: true }))
     .setTimestamp();
