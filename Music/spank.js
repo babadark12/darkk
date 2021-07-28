6bc5d440040aa96971a0320beb8f11b5dff88bd2
/MrRobot <87871568+kakr0b0t@users.noreply.github.com>
2021-07-28 13:06:27 +0300

    Create spank.js

---
+++ Music/spank.js
@@ -0,0 +1,27 @@
+const { MessageEmbed } = require("discord.js");
+const fetch = require("node-fetch");
+const { lineReply } = require("discord-reply");
+
+module.exports = {
+  name: "spank",
+  description: "Spank someone Σ(っﾟДﾟ；)っ",
+  category: "action",
+  usage:"spank @user",
+ async execute(message, args, client) {
+    const data = await fetch("https://nekos.life/api/v2/img/spank").then((res) =>
+      res.json()
+    );
+    const user = message.mentions.users.first() || message.author;
+    const spanked = message.author.id === user.id ? "themselfs" : user.username;
+
+    const embed = new MessageEmbed()
+      .setTitle(`${message.author.username} Spanked ${spanked}`)
+      .setFooter(message.author.username)
+      .setColor("#0d2943")
+      .setDescription(`[Click here if the image failed to load.](${data.url})`)
+      .setImage(`${data.url}`)
+      .setTimestamp();
+
+    message.lineReplyNoMention(embed);
+  },
+};
