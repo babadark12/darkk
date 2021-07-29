efe2ff51c501baea49699e9ed31ed70884667482
/MrRobot <87871568+kakr0b0t@users.noreply.github.com>
2021-07-29 21:17:27 +0300

    Update info.js

--- Music/info.js
+++ Music/info.js
@@ -8,13 +8,13 @@ module.exports = {
   usage: "info",
   async execute(message, args, client) {
         const embed = new MessageEmbed()
-        .setTitle("Akeno Info")
+        .setTitle("Creator Info")
         .addField("Creator:", "MrRobot#7265")
         .addField("Youtube:", "[click here](https://youtube.com/channel/UC_EfAg6_maZeTyQN2T-0pzg)")
         .addField("Discord:", "[click here](https://discord.gg/mjyZB4deSh)")
         .addField("Language:", "discord.js")
         .setFooter("To View List Of Command Type +help")
-        .setColor("#0d2943")
+        .setColor("#116d56")
        message.lineReplyNoMention(embed);
 
 }}
