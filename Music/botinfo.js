da33d537974b48be26426884c40389558fb95537
/MrRobot <87871568+kakr0b0t@users.noreply.github.com>
2021-07-29 19:33:10 +0300

    Update botinfo.js

--- Music/botinfo.js
+++ Music/botinfo.js
@@ -7,15 +7,14 @@ module.exports = {
   description: "INVITE BOT",
   async execute(message, args, client) {
     let embed = new discord.MessageEmbed()
-      .setTitle(`__**INFORMATION ABOUT BOT**__`)
+      .setTitle(`Rupo Inforimation`)
       .addField("Bot Name", `<@${client.user.id}>`)
       .addField("Bot Developer", `@MrRobot#7265`)
-      .addField("Total Guilds", `${client.guilds.cache.size}`, true)
-      .addField("Total Channels", `${client.channels.cache.size}`)
-      .addField("Total Users", `${client.guilds.cache.reduce((a, g) => a + g.memberCount, 0)}`, true)
+      .addField("Guilds", `${client.guilds.cache.size}`, true)
+      .addField("Channels", `${client.channels.cache.size}`)
+      .addField("Users", `${client.guilds.cache.reduce((a, g) => a + g.memberCount, 0)}`, true)
       .addField("Bot Libary", `discord.js`)
-      .setColor("#0d2943")
-      .setFooter(`information about bot`);
+      .setColor("#985db3")
       message.lineReplyNoMention(embed).catch(console.error);
   }
 };
