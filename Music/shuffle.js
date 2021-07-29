b8d824bea7852fcde3abbbe08ac17d16db9d00a2
/MrRobot <87871568+kakr0b0t@users.noreply.github.com>
2021-07-29 21:36:37 +0300

    Update shuffle.js

--- Music/shuffle.js
+++ Music/shuffle.js
@@ -33,7 +33,7 @@ execute(message) {
     queue.connection.dispatcher.end();
     //send approve message
     queue.textChannel.send(
-      new MessageEmbed().setColor("#c219d8").setAuthor(`${message.author.username} skipped the song.`, "https://cdn.discordapp.com/emojis/769915194444480542.png")
+      new MessageEmbed().setColor("#116d56").setAuthor(`${message.author.username} skipped the song.`)
     ).catch(console.error);
   }
 };
