a52d3993b9a66fa363455b971a2807d74386dcf1
/MrRobot <87871568+kakr0b0t@users.noreply.github.com>
2021-07-29 21:36:08 +0300

    Update servers.js

--- Music/servers.js
+++ Music/servers.js
@@ -8,7 +8,7 @@ module.exports = {
   usage: "status",
   async execute(message, args, client) {
     const embed = new Discord.MessageEmbed()
-        .setColor("#0d2943")
+        .setColor("#116d56")
         .addField("Guilds Count", `\`${client.guilds.cache.size}\``, true)
         .addField("Users Count", `\`${client.guilds.cache.reduce((a, g) => a + g.memberCount, 0)}\``, true)
         .addField("Channel Count", `\`${client.channels.cache.size}\``, true)
