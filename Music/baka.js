5e5587a04b6ededf8f07ec72ac82831d04a9cb6d
/MrRobot <87871568+kakr0b0t@users.noreply.github.com>
2021-07-29 19:29:17 +0300

    Update baka.js

--- Music/baka.js
+++ Music/baka.js
@@ -13,7 +13,7 @@ module.exports = {
       const embed = new Discord.MessageEmbed()
       .setTitle("BAKA!!!")
       .setImage(response.body.url)
-      .setColor(`RANDOM`)
+      .setColor(`#985db3`)
       .setFooter(`idiot!`)
       .setURL(response.body.url);
   message.lineReplyNoMention(embed);
