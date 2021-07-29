74457aefca7c43b4b1f3b3bd0e0bc910dac21d08
/MrRobot <87871568+kakr0b0t@users.noreply.github.com>
2021-07-29 22:47:42 +0300

    Update invites.js

--- Music/invites.js
+++ Music/invites.js
@@ -1,17 +1,12 @@
-const fs = require("fs");
-const Discord = require("discord.js");
+const Discord = require('discord.js');
 const { lineReply } = require("discord-reply");
 
 module.exports = {
-  name: "invites",
-  aliases: ["userinvites"],
-  enabled: true,            
-  memberPermissions: [ "SEND_MESSAGES" ],            
-  botPermissions: [ "SEND_MESSAGES", "EMBED_LINKS" ],        
-  ownerOnly: false,            
-  cooldown: 5,
-   async execute(message, args) {
-
+        name:"invites",
+        aliases: ["invs"],
+        category: "info",
+        accessableby: "",
+ async execute(message, args, client) {
         try {
             let member = await message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(r => r.user.username.toLowerCase() === args.join(' ').toLocaleLowerCase()) || message.guild.members.cache.find(r => r.displayName.toLowerCase() === args.join(' ').toLocaleLowerCase()) || message.member;
 
@@ -20,7 +15,7 @@ module.exports = {
             let memberInvites = invites.filter(i => i.inviter && i.inviter.id === member.user.id);
 
             if (memberInvites.size <= 0) {
-                return message.channel.send(`**${member.displayName} didn't invite anyone to the server!**`, (member === message.member ? null : member));
+                return message.lineReplyNoMention(`**${member.displayName} didn't invite anyone to the server!**`, (member === message.member ? null : member));
   {}          }
 
             let content = memberInvites.map(i => i.code).join("\n");
@@ -28,13 +23,12 @@ module.exports = {
             memberInvites.forEach(invite => index += invite.uses);
 
             let embed = new Discord.MessageEmbed()
-                .setColor("#0d2943")
-                .setAuthor(`${message.guild.name}`,message.guild.iconURL({ dynamic: true }))
-                .setThumbnail(message.author.avatarURL({dynamic: "true"}))
-                .setDescription(`Information on Invites of ${member.displayName}`)
-                .setFooter(`${message.author.username}#${message.author.discriminator}`, message.member.user.displayAvatarURL({ dynamic: true }))
-                .addField("**No. Invited Persons**", index)
-                .addField("Invitation Codes\n\n", content);
+                .setColor("#116d56")
+                .setFooter(message.guild.name, message.guild.iconURL())
+                .setAuthor(`**🎈 | Invites Tracker For ${message.guild.name}**`)
+                .setDescription(`**ℹ️ | Information on Invites of ${member.displayName}**`)
+                .addField("**❌ | No. Invited Persons**", index)
+                .addField("**🖇️ | Invitation Codes\n\n**", content);
             message.lineReplyNoMention(embed);
         } catch (e) {
             return message.channel.send(e.message)
