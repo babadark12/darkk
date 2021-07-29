f37faf806c3f7da0fa20e91501a8fe6d4ea981cf
/MrRobot <87871568+kakr0b0t@users.noreply.github.com>
2021-07-29 22:58:20 +0300

    Update botinfo.js

--- Music/botinfo.js
+++ Music/botinfo.js
@@ -8,12 +8,12 @@ module.exports = {
   async execute(message, args, client) {
     let embed = new discord.MessageEmbed()
       .setTitle(`Rupo Inforimation`)
-      .addField("Name", `<@${client.user.id}>`)
-      .addField("Creator", `@MrRobot#7265`)
-      .addField("Guilds", `${client.guilds.cache.size}`, true)
-      .addField("Channels", `${client.channels.cache.size}`)
-      .addField("Users", `${client.guilds.cache.reduce((a, g) => a + g.memberCount, 0)}`, true)
-      .addField("Bot Libary", `discord.js`)
+      .addField("❯ Name", `<@${client.user.id}>`)
+      .addField("❯ Creator", `@MrRobot#7265`)
+      .addField("❯ Guilds", `${client.guilds.cache.size}`, true)
+      .addField("❯ Channels", `${client.channels.cache.size}`)
+      .addField("❯ Users", `${client.guilds.cache.reduce((a, g) => a + g.memberCount, 0)}`, true)
+      .addField("❯ Language", `java.script`)
       .setColor("#116d56")
       message.lineReplyNoMention(embed).catch(console.error);
   }
