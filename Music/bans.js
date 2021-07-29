c0c4aa85286731f58f3a1187a82b9c81387338dc
/MrRobot <87871568+kakr0b0t@users.noreply.github.com>
2021-07-29 19:32:20 +0300

    Update bans.js

--- Music/bans.js
+++ Music/bans.js
@@ -21,7 +21,7 @@ if (!message.channel.guild) return;
 		message.guild.fetchBans().then(bans =>
 message.lineReplyNoMention (new MessageEmbed()
 .setAuthor(message.guild.name, message.guild.iconURL())
-.setColor("#0d2943")
+.setColor("#985db3")
 .addField("Total Ban's Is", `\`${bans.size}\``)
 .setFooter(`${message.author.tag}`, message.author.avatarURL()))).catch(console.error);
     }
