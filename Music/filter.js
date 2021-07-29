8c729aed101bad8fc019907d70d451b976674895
/MrRobot <87871568+kakr0b0t@users.noreply.github.com>
2021-07-29 21:14:04 +0300

    Update filter.js

--- Music/filter.js
+++ Music/filter.js
@@ -91,7 +91,7 @@ async execute(message, args, client) {
         //fires if not valid input
         varforfilter = 404;
         message.channel.send(new MessageEmbed()
-        .setColor("#985db3")
+        .setColor("#116d56")
         .setTitle("Not a valid Filter, use one of those:")
         .setDescription(`
         \`bassboost\`
@@ -116,7 +116,7 @@ async execute(message, args, client) {
       const song = queue.songs[0];
       //play the collected song song, message, client, filters
       message.channel.send(new MessageEmbed()
-      .setColor("#985db3")
+      .setColor("#116d56")
       .setAuthor("Applying: " + args[0], "https://cdn.discordapp.com/emojis/769935094285860894.gif")).then(msg =>{
         msg.delete({timeout: 2000});
       })
