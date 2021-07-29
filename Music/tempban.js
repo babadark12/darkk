430062618fd397d059deb06b3d91089a7c612aa8
/MrRobot <87871568+kakr0b0t@users.noreply.github.com>
2021-07-29 21:40:56 +0300

    Update tempban.js

--- Music/tempban.js
+++ Music/tempban.js
@@ -66,14 +66,14 @@ module.exports = {
       }
       if (reason) {
         var sembed = new MessageEmbed()
-          .setColor("#0d2943")
+          .setColor("#116d56")
           .setDescription(
             `**${banMember.user.username}** has been banned for ${reason} - Time ${regex}`
           );
         message.channel.send(sembed);
       } else {
         var sembed2 = new MessageEmbed()
-          .setColor("#0d2943")
+          .setColor("#116d56")
           .setDescription(
             `**${banMember.user.username}** has been banned - Time ${regex}`
           );
@@ -86,7 +86,7 @@ module.exports = {
 
       const embed = new MessageEmbed()
         .setAuthor(`${message.guild.name} Modlogs`, message.guild.iconURL())
-        .setColor("##0d2943")
+        .setColor("#116d56")
         .setThumbnail(banMember.user.displayAvatarURL({ dynamic: true }))
         .setFooter(message.guild.name, message.guild.iconURL())
         .addField("**Moderation**", "tempban")
