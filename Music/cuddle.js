e79bda978ea06a4ec3b9ff4b6796a26856e74dfb
/MrRobot <87871568+kakr0b0t@users.noreply.github.com>
2021-07-29 21:12:25 +0300

    Update cuddle.js

--- Music/cuddle.js
+++ Music/cuddle.js
@@ -21,7 +21,7 @@ module.exports = {
               const embed = new Discord.MessageEmbed()
               .setTitle(user.username + " Just got a cuddle from " + message.author.username)
               .setImage(response.body.url)
-              .setColor(`#985db3`)
+              .setColor(`#116d56`)
               .setDescription((user.toString() + " got a cuddle from " + message.author.toString()))
               .setFooter(`this is so cute`)
               .setURL(response.body.url);
