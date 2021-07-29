638bb9d688f2cb96f7a518080f31f77e2edbfa21
/MrRobot <87871568+kakr0b0t@users.noreply.github.com>
2021-07-29 21:22:10 +0300

    Update lyrics.js

--- Music/lyrics.js
+++ Music/lyrics.js
@@ -32,7 +32,7 @@ async execute(message) {
     //define the temporary Embed
     let temEmbed = new MessageEmbed()
     .setAuthor("Searching...", "https://cdn.discordapp.com/emojis/757632044632375386.gif?v=1").setFooter("Lyrics")
-    .setColor("#f300e5")
+    .setColor("#116d56")
     //send it and safe it in a variable
     let result = await message.channel.send(temEmbed)
     //try to find lyrics
@@ -50,7 +50,7 @@ async execute(message) {
     let lyricsEmbed = new MessageEmbed()
       .setTitle("Lyrics")
       .setDescription(lyrics)
-      .setColor("#f300e5")
+      .setColor("#116d56")
     //if to long make slice it
     if (lyricsEmbed.description.length >= 2048)
       //slice the embed description and redefine it
