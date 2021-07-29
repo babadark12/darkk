50ca21ce2969b1bcea5428dfea567117091a1543
/MrRobot <87871568+kakr0b0t@users.noreply.github.com>
2021-07-29 21:09:52 +0300

    Update cinfo.js

--- Music/cinfo.js
+++ Music/cinfo.js
@@ -20,7 +20,7 @@ module.exports = {
             .addField("**Channel Type**", channel.type)
             .addField("**Channel Description**", `${channel.topic || "No Description"}`)
             .addField("**Channel Created At**", channel.createdAt)
-            .setColor("#985db3")
+            .setColor("#116d56")
         message.lineReplyNoMention(channelembed);
     }
 }
