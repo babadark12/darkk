835a28c80ee31798313a4a2d2e69d8f01e98dd9d
/MrRobot <87871568+kakr0b0t@users.noreply.github.com>
2021-07-29 15:43:31 +0300

    Create antispam.js

---
+++ Music/antispam.js
@@ -0,0 +1,40 @@
+const db = require('quick.db')
+
+module.exports = {
+  name: 'antispam',
+  description: 'Toggles the anti spam module for the server',
+  aliases: ['as', 'spam'],
+  usage: ' [on/off]',
+  cooldown: 2,
+  args: 1,
+  catergory: 'Server Settings',
+  async execute(message, args, client) {
+    try {
+      let Toggle = args[0].toLowerCase();
+      if (Toggle != "off" && Toggle != "on") {
+        return message.reply("Please choose a option, on or off!");
+      }
+
+      if (!message.member.hasPermission('ADMINISTRATOR')) {
+        return message.reply("Please make sure you have administrator perms!")
+      }
+
+      db.set(`${message.guild.id}_antispam`, Toggle);
+      return message.channel.send({
+        embed: {
+          title: "Server Anti-Spam",
+          description: `**Status:** ${Toggle}`,
+          color: "#0d2943",
+          footer: {
+            text: "Requested by " + message.author.tag,
+            icon_url: message.author.displayAvatarURL()
+          },
+          timestamp: new Date()
+        }
+      });
+    } catch (err) {
+      console.log(err);
+      return message.reply(`Oh no, an error occurred. Try again later!`);
+    }
+  }
+};
