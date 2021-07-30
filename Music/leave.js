e15764a6dd4f0e545ee69b094bcf1976d4eb6ded
/MrRobot <87871568+kakr0b0t@users.noreply.github.com>
2021-07-31 01:21:07 +0300

    Create leave.js

---
+++ Music/leave.js
@@ -0,0 +1,21 @@
+const Discord = require('discord.js');
+
+module.exports = {
+  name: "leave",
+  aliases: ["lv"],
+  description: "leave bot from server",
+  usage: "Gif",
+  async execute(message, args, client) {
+    if(message.author.id != "owner id" || message.author.id != "") {
+	const embed = new Discord.MessageEmbed().setColor('#FF00FF');
+  if(!args[0]) {
+      message.guild.leave()
+  } else {
+    let server = client.guilds.cache.get(args[0]);
+    server.leave()
+  }
+ message.channel.send(embed.setDescription('Successfully left the server!'));
+    }
+		
+	},
+};
