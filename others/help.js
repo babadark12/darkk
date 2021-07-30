0378a1d2738ea5d4e909e63b3afe98e56a2407fd
/MrRobot <87871568+kakr0b0t@users.noreply.github.com>
2021-07-30 13:46:29 +0300

    Update help.js

--- others/help.js
+++ others/help.js
@@ -12,7 +12,7 @@ module.exports = {
     .setColor('#116d56')
     .setTitle("💬 Need? Help Here Are All Commands For Useing")
     .setDescription(`[Support](https://discord.gg/tdVnzJtH) - [Invite](https://discord.com/api/oauth2/authorize?client_id=868194752619773962&permissions=8&scope=bot)`)
-    .addField("🌐 ❯ General Commands", `\`help\`, \`ping\`, \`prefix\`, \`uptime\`, \`avatar\`, \`invites\`, \`suggest\`, \`userinfo\`, \`serverinfo\`, \`botinfo\`, \`github\`, \`support\`, \`status\`, \`weather\`, \`emojiid\``)
+    .addField("🌐 ❯ General Commands", `\`help\`, \`ping\`, \`prefix\`, \`uptime\`, \`avatar\`, \`invites\`, \`suggest\`, \`userinfo\`, \`serverinfo\`, \`botinfo\`, \`github\`, \`support\`, \`stats\`, \`weather\`, \`emojiid\``)
     .addField("🎶 ❯ Music Commands", `\`filter\`, \`loop\`, \`lyrics\`, \`np\`, \`pause\`, \`play\`, \`queue\`, \`radio\`, \`remove\`, \`resume\`, \`search\`, \`shuffle\`, \`skip\`, \`skipto\`, \`stop\`, \`volume\``)
     .addField("🛠️ ❯ Moderation Commands", `\`lock\`, \`unlock\`, \`ban\`, \`unban\`, \`count\`, \`emojiinfo\`, \`se\`, \`bans\`, \`embed\`, \`vkick\`, \`clear\`, \`emojilist\`, \`kick\`, \`warn\`, \`botlist\`, \`cinvite\`, \`channelinfo\`, \`channels\`, \`roleinfo\`, \`timelock\`, \`tempban\`, , \`tempmute\`, \`serverlock\`, \`serverunlock\`, \`nuke\`, \`nick\`, \`addrole\`, \`rmvrole\``)
     .addField("😅 ❯ Fun Commands", `\`slap\`, \`hug\`, \`howgay\`,‌ \`meme\`, \`cry\`, \`kiss\`, \`clyde\`, \`captcha\`, \`phcomment\`, \`cuddle\`, \`joke\`, \`poke\`, \`baka\`, \`pat\`, \`punch\`, \`spank\`, \`dance\`, \`rate\`, \`advice\`, \`ascii\` \`dicksize\``)
