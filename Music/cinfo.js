d6de92c4c7f0f962c6ebd2d8ae8a3f3e628317b1
/MrRobot <87871568+kakr0b0t@users.noreply.github.com>
2021-07-31 12:23:52 +0300

    Update cinfo.js

--- Music/cinfo.js
+++ Music/cinfo.js
@@ -2,25 +2,70 @@ const { MessageEmbed } = require('discord.js');
 const { lineReply } = require("discord-reply");
 
 module.exports = {
-        name: "channelinfo",
-        aliases: ['ci', 'channeli', 'cinfo'],
-        category: "info",
-        description: "Shows Channel Info",
-        usage: "[ channel mention | channel name | ID] (optional)",
-        accessableby: "everyone",
-       async execute(message, args, client) {
-        let channel = message.mentions.channels.first() || bot.guilds.cache.get(message.guild.id).channels.cache.get(args[0]) || message.guild.channels.cache.find(r => r.name.toLowerCase() === args.join(' ').toLocaleLowerCase()) || message.channel;
-        if (!channel) return message.channel.send("**Channel Not Found!**");
+    name: "channelinfo",
+    category: "Music",
+    aliases: ["chatinfo", "cinfo"],
+    description: "Adds the role to the mentioned user or ID with mentioned role or ID !!",
+    example: `+channelinfo #channel`,
+    async execute(message, args, client) {
+         
+	let channel = message.mentions.channels.first() || message.guild.channels.cache.get(args[0]) || message.guild.channels.cache.find(Ch => Ch.name.toLowerCase() === args.join(" ").toLowerCase()) || message.guild.channels.cache.find(Ch => Ch.name.toLowerCase().includes(args.join(" ").toLowerCase())) || message.guild.channels.cache.find(Ch => Ch.name.toLowerCase().startsWith(args.join(" ").toLowerCase())) || message.guild.channels.cache.find(Ch => Ch.name.toLowerCase().endsWith(args.join(" ").toLowerCase()));
+if(!channel || channel.type === "unknown" || !args.length) return message.channel.send("Please Mention A Valid Channel Or Name Or ID Or Channel Starts/Ends With Or Channel Includes!");
 
-        let channelembed = new MessageEmbed()
-            .setTitle(`Channel Information for ${channel.name}`)
-            .setThumbnail(message.guild.iconURL())
-            .addField("**Channel Nfsw**", channel.nsfw, true)
-            .addField("**Channel ID**", channel.id, true)
-            .addField("**Channel Type**", channel.type)
-            .addField("**Channel Description**", `${channel.topic || "No Description"}`)
-            .addField("**Channel Created At**", channel.createdAt)
-            .setColor("#116d56")
-        message.lineReplyNoMention(channelembed);
-    }
+if(channel.type === "voice") {
+  
+const channelEmbed = new MessageEmbed()
+.setThumbnail(message.guild.iconURL)
+.setDescription([
+`**❯ Name:** ${channel.name}`, 
+`**❯ ID:** ${channel.id}`, 
+`**❯ Type:** ${channel.type.charAt(0).toUpperCase() + channel.type.slice(1)}`, 
+`**❯ Creation Date:** ${channel.createdAt.toDateString()}`, 
+`**❯ Category:** ${channel.parent.name || 'None'}`, 
+`**❯ Bitrate:** ${channel.bitrate}`, 
+`**❯ User Limit:** ${channel.userLimit === 0 ? "Unlimited" : channel.userLimit}`, 
+`**❯ Full:** ${channel.full ? 'Yes' : 'No' }`, 
+`**❯ Members:** ${channel.members ? channel.members.array().length : 0}
+${channel.members ? channel.members.array().join(" **|** ") : 'None'}`
+]) 
+.setTimestamp()
+.setColor('RANDOM')
+.setFooter(`Requested by ${message.author.tag}`, message.author.displayAvatarURL({dynamic: true}))
+message.lineReplyNoMention(`**Information About** <#${channel.id}> **Channel**`,channelEmbed)
+}
+else if((channel.type === "text") || (channel.type === "news") || (channel.type === "store")) {
+const channelEmbed = new MessageEmbed()
+.setThumbnail(message.guild.iconURL)
+.setDescription([
+`**❯ Name:** ${channel.name}`, 
+`**❯ ID:** ${channel.id}`, 
+`**❯ Type:** ${channel.type.charAt(0).toUpperCase() + channel.type.slice(1)}`, 
+`**❯ Creation Date:** ${channel.createdAt.toDateString()}`, 
+`**❯ NSFW:** ${channel.nsfw ? 'Yes' : 'No'}`, 
+`**❯ Category:** ${channel.parent ? channel.parent.name : 'None'}`, 
+`**❯ Topic:** \n${channel.topic ? channel.topic.length > 200 ? `${channel.topic.slice(0, 200)}...` : channel.topic : "None"}`
+]) 
+.setColor('RANDOM') 
+.setTimestamp() 
+.setFooter(`Requested by ${message.author.tag}`, message.author.displayAvatarURL({dynamic: true}))
+message.lineReplyNoMention(`**Information About** <#${channel.id}> **Channel**`,channelEmbed)
+
+}
+else {
+const channelEmbed = new MessageEmbed()
+.setThumbnail(message.guild.iconURL)
+.setDescription([
+`**❯ Name:** ${channel.name}`, 
+`**❯ ID:** ${channel.id}`, 
+`**❯ Type:** ${channel.type.charAt(0).toUpperCase() + channel.type.slice(1)}`, 
+`**❯ Creation Date:** ${channel.createdAt.toDateString()}`, 
+`**❯ Channels**: ${channel.children ? channel.children.array().length : 0}
+${channel.children ? channel.children.array().join(" **|** ") : 'None'}`
+		])
+.setTimestamp()
+.setColor('RANDOM')
+.setFooter(`Requested by ${message.author.tag}`, message.author.displayAvatarURL({dynamic: true}))
+	message.lineReplyNoMention(`**Information About** <#${channel.id}> **Channel**`,channelEmbed)
+}
+	}
 }
