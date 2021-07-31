249a81a3396a9467ee531ef0cb843a4bfd88138f
/MrRobot <87871568+kakr0b0t@users.noreply.github.com>
2021-07-31 14:11:24 +0300

    Create snake.js

---
+++ Music/snake.js
@@ -0,0 +1,30 @@
+const { Snake } = require('weky')
+
+module.exports = {
+        name: 'snake',
+        aliases: ['snake'],
+        description: 'Play the snake game in Discord!',
+        category: __dirname.split("Commands\\")[1],
+    async execute(message, args, client) {
+
+        const game = new Snake({
+            message: message,
+            embed: {
+                title: 'Snake',
+                color: config.embedcolor,
+                gameOverTitle: "Game Over",
+            },
+            emojis: {
+                empty: '⬛',
+                snakeBody: '🐍',
+                food: '🍎 ',
+                //controls
+                up: '⬆️',
+                right: '⬅️',
+                down: '⬇️',
+                left: '➡️'
+            }
+        })
+        game.start()
+    }
+}
