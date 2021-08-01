8a721d35b79b3193b2b4940f5d3a5af973174f27
/MrRobot <87871568+kakr0b0t@users.noreply.github.com>
2021-08-01 13:04:23 +0300

    Update emoji.js

--- Music/emoji.js
+++ Music/emoji.js
@@ -1,34 +1,41 @@
-const Discord = require('discord.js')
-const moment = require('moment')//npm i moment
-const { lineReply } = require("discord-reply");
+const { Client, Message, MessageEmbed } = require("discord.js");
+const moment = require("moment");
+const { lineReply } = require("discord-reply"); 
 
 module.exports = {
-    name: "emojiinfo",
-    aliases: ["emoteid"],
-    usage: "emojiinfo <emoji name, without ::>",
-    category: "info",
-    description: "Show emojiinfo of emoji",
-        async execute(message, args, client) {
-        let emojiName = args.join(" ");
-        let emoji = message.guild.emojis.cache.get(args[0]) || message.guild.emojis.cache.find(emoji => emoji.name === `${emojiName}`) 
-        if (!args[0]) return message.lineReplyNoMention("Please provide an emoji name or id!")
-        if (!emoji) return message.lineReplyNoMention("Could not find emoji... Please provide emoji name or emoji id!")
-        let xd;
-        if(emoji.animated) xd = "<:yes_17:871319322792976455>"
-        if(!emoji.animated) xd = '<:no_18:871319342170656780>'
-        let embed = new Discord.MessageEmbed()
+  name: "emojiinfo",
+  aliases: ["ei", "emi"],
+  description: "Shows information about an emoji!",
+  usage: "<emoji>",
+  async execute(message, args, client) {
+    const emote = args[0];
+    if (!emote) return message.lineReplyNoMention({content: `Please provide an emoji!`});
+    const regex = emote.replace(/^<a?:\w+:(\d+)>$/, "$1");
 
-            .addField("Name", `${emoji.name}`)
-            .addField("Emoji id", `${emoji.id}`)
-            .addField("Outlook", `${emoji}`)
-            .addField("Created at", `${moment(emoji.createdTimestamp).format('LT')} ${moment(emoji.createdTimestamp).format('LL')} ${moment(emoji.createdTimestamp).fromNow()}`)
-            .addField("Guild", message.guild.name)
-            .addField("ANIMATED", xd)
-            .setThumbnail(emoji.url)
-            .setColor("#116d56")
-            .addField("Format", `\`<:${emoji.name}:${emoji.id}>\``)
-            .addField("URL", `[click here](${emoji.url})`)
+    const emoji = message.guild.emojis.cache.find(
+      (emoj) => emoj.name === emote || emoj.id === regex
+    );
+    if (!emoji)
+      return message.lineReplyNoMention({content: `Please provide an emoji!`});
 
-        message.lineReplyNoMention(embed)
-    }
-}
+    const embed = new MessageEmbed()
+      .setTitle(`${emoji.name} ${emoji}`)
+      .addField(`Info`, [
+        `🆔 Emoji Id: ${emoji.id}`,
+        `🔗 Emoji Url: [Click Here](${emoji.url})`,
+        `🎬 Animated: ${
+          emoji.animated ? "Yes" : "No"}`,
+        `➕ Emoji Added By: ${emoji.author}`,
+        `🧭 Emoji Added At: ${emoji.createdAt}`,
+        `🤔 Requires Colon: ${emoji.requiresColons ? "Yes" : "No"}`,
+        `📜 Managed Emoji? : ${emoji.managed ? "Yes" : "No"}`,
+        `❔ Deleteable: ${
+          emoji.deletable ? "Yes" : "No"
+        }`,
+      ])
+      .setThumbnail(emoji.url)
+      .setColor("#116d56");
+
+    message.lineReplyNoMention({embeds: [embed]});
+  },
+};
