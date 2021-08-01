adf231bc01048ab05af827ae71e4abae79eeb55d
/MrRobot <87871568+kakr0b0t@users.noreply.github.com>
2021-08-01 12:17:37 +0300

    Update botinfo.js

--- Music/botinfo.js
+++ Music/botinfo.js
@@ -1,20 +1,31 @@
-const discord = require("discord.js");
-const { lineReply } = require("discord-reply");
+const db = require("quick.db");
+const Discord = require ("discord.js")
+const { version } = require('../../package.json');
+const ms = require('pretty-ms');
+const { version: discordjsVersion } = require('discord.js');
 
 module.exports = {
-  name: "botinfo",
-  category: "info",
-  description: "INVITE BOT",
+  name: 'about',
+  usage: '',
+  aliases: ['aboutbot', 'botinfo'],
+  description: `About zAltmr`,
+
   async execute(message, args, client) {
-    let embed = new discord.MessageEmbed()
-      .setTitle(`Rupo Inforimation`)
-      .addField("❯ Name", `<@${client.user.id}>`)
-      .addField("❯ Creator", `@MrRobot#7265`)
-      .addField("❯ Guilds", `${client.guilds.cache.size}`, true)
-      .addField("❯ Channels", `${client.channels.cache.size}`)
-      .addField("❯ Users", `${client.guilds.cache.reduce((a, g) => a + g.memberCount, 0)}`, true)
-      .addField("❯ Language", `java.script`)
-      .setColor("#116d56")
-      message.lineReplyNoMention(embed).catch(console.error);
+    message.channel.send(new Discord.MessageEmbed()
+            .setColor('#116d56')
+            .setTitle(`Rupo v${version}`)
+            .setThumbnail(client.user.displayAvatarURL({ dynamic: true }))
+            .addField('💭 Prefix', `\`+\``, true)
+            .addField('🕕 Uptime', `\`${ms(client.uptime)}\``, true)
+            .addField('🤖 Ping', `\`${client.ws.ping}ms\``, true)
+            .addField('💾 Memory', `\`${(process.memoryUsage().rss / 1024 / 1024).toFixed(2)} MB RSS\n${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB Heap\``, true)
+            .addField('🏠 Guild Count', `\`${client.guilds.cache.size}\` guilds`, true)
+            .addField('💬 Commands', `\`${client.commands.size}\` cmds`,true)
+            .addField('💻 Node', `\`${process.version} on ${process.platform} ${process.arch}\``, true)
+            .addField('💾 Cached Data', `\`${client.users.cache.size} users\n${client.emojis.cache.size} emojis\``, true)
+            .addField('💻 Discord.js', `\`${discordjsVersion}\``, true)
+            .setTimestamp()
+            .setThumbnail(client.user.displayAvatarURL({ dynamic: true }))
+        );
   }
-};
+}
