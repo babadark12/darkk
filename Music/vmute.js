44f79de5a9445009e4b27a05f574cd50978929c6
/MrRobot <87871568+kakr0b0t@users.noreply.github.com>
2021-08-01 13:23:12 +0300

    Create vmute.js

---
+++ Music/vmute.js
@@ -0,0 +1,31 @@
+const Discord = require("discord.js");
+const { lineReply } = require("discord-reply"); 
+
+module.exports = {
+  name: "vmute",
+  description: "voicemute a user",
+  category: "admin",
+  botPermissions: ["MUTE_MEMBERS"],
+  memberPermissions: ["MUTE_MEMBERS"],
+  async execute(bot, message, args) {
+    const muteUser = message.guild.member(
+      message.mentions.users.first() || message.guild.members.cache.get(args[0])
+    );
+    const muteReason = args.join(" ").slice(23);
+
+    if (muteUser.voice.serverMute) {
+      return message.lineReplyNoMention(
+        "**User is not in a voice channel or is already muted**"
+      );
+    }
+
+    muteUser.voice.setMute(true, "muteReason");
+
+    muteUser.user.send(
+      `You've been **Muted** from **${message.guild.name}**, Reason: **${muteReason}**`
+    );
+    message.lineReplyNoMention(
+      `**${muteUser.user.tag}** was successfully muted from the server. Reason: **${muteReason}**. I have also send a DM letting the person know.`
+    );
+  },
+};
