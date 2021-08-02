e743b2da67f1ef43c5681df21c4887717996ab33
/MrRobot <87871568+kakr0b0t@users.noreply.github.com>
2021-08-02 20:40:37 +0300

    Update index.js

--- index.js
+++ index.js
@@ -166,14 +166,12 @@ Type \`${prefix}help\` for the list of commands.`));
 
 client.on("guildCreate", guild => {
   let channel = client.channels.cache.get("871701453939560488");
-  let embed = new MessageEmbed().setColor("#116d56")
-  .setAuthor(client.user.username, client.user.avatarURL())
-  .setTitle( `✅ Join Server`)
-  .addField(" **Server Name**", `${guild.name}`)
-  .addField(" **Server Owner**", `${guild.owner}`)
-  .addField(" **Server Id**", `${guild.id}`)
-  .addField(" **Member Count**", `${guild.memberCount}`)
-  .setFooter(`${client.user.tag}`);
+  let embed = new MessageEmbed()
+  .setColor('GREEN')
+  .setDescription(`I have joined the ${guild.name} server.\n\nID: ${guild.id}`)
+  .setFooter(`Gained ${guild.members.cache.size - 1} members • I'm now in ${this.client.guilds.cache.size} servers!`)
+  .setThumbnail(guild.iconURL({ dynamic: true }) ? guild.iconURL({ dynamic: true }) : `https://guild-default-icon.herokuapp.com/${encodeURIComponent(guild.nameAcronym)}`)
+  .addField('Server Owner', `${guild.owner.user.tag} / ${guild.ownerID}`)
   channel.send(embed);
 });
 
