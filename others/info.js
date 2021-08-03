fc7949490a10811adbc186dd38bc85bd2ac9f779
/MrRobot <87871568+kakr0b0t@users.noreply.github.com>
2021-08-03 15:03:10 +0300

    Update and rename other.js to info.js

--- others/info.js
+++ others/info.js
@@ -3,15 +3,15 @@ const { lineReply } = require("discord-reply");
 const prefix = ("+");
 
 module.exports = {
-  name: "Other",
-  aliases: ["other", "o"],
+  name: "Info",
+  aliases: ["info", "i"],
   description: "Display all commands and descriptions",
   execute(message) {
     let commands = message.client.commands.array();
      
     let helpEmbed = new MessageEmbed()
     .setColor('#116d56')
-    .setDescription(`Invite RupoBot To Your Server By[click here](https://discord.com/api/oauth2/authorize?client_id=868194752619773962&permissions=8&scope=bot)\n\nThere All Other Commands List \n\n${prefix}help, ${prefix}ping, ${prefix}prefix, ${prefix}uptime, ${prefix}avatar, ${prefix}invites, ${prefix}suggest, ${prefix}userinfo, ${prefix}serverinfo, ${prefix}github, ${prefix}support, ${prefix}botinfo, ${prefix}weather, ${prefix}embed`)
+    .setDescription(`Invite RupoBot To Your Server By[click here](https://discord.com/api/oauth2/authorize?client_id=868194752619773962&permissions=8&scope=bot)\n\nThere All Info Commands List \n\n${prefix}help, ${prefix}ping, ${prefix}prefix, ${prefix}uptime, ${prefix}avatar, ${prefix}invites, ${prefix}suggest, ${prefix}userinfo, ${prefix}serverinfo, ${prefix}github, ${prefix}support, ${prefix}botinfo, ${prefix}weather, ${prefix}embed`)
     .setFooter(`Requested by ${message.author.tag}`, message.author.displayAvatarURL({dynamic: true}))
     helpEmbed.setTimestamp();
 
