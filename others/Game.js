c173765709ea67025ddab3c3cbd1841380d1c799
/MrRobot <87871568+kakr0b0t@users.noreply.github.com>
2021-08-03 14:15:00 +0300

    Create Game.js

---
+++ others/Game.js
@@ -0,0 +1,20 @@
+const { MessageEmbed } = require("discord.js");
+const { lineReply } = require("discord-reply");
+const prefix = ("+");
+
+module.exports = {
+  name: "Game",
+  aliases: ["game"],
+  description: "Display all commands and descriptions",
+  execute(message) {
+    let commands = message.client.commands.array();
+     
+    let helpEmbed = new MessageEmbed()
+    .setColor('#116d56')
+    .setDescription(`Invite RupoBot To Your Server By [click here](https://discord.com/api/oauth2/authorize?client_id=868194752619773962&permissions=8&scope=bot)\n\nThere All Fun Commands List \n\n${prefix}rps, ${prefix}slots, ${prefix}iq, ${prefix}hack, ${prefix}8ball`)
+    .setFooter(`${message.author.tag}`, message.author.avatarURL)
+    helpEmbed.setTimestamp();
+
+    message.lineReplyNoMention(helpEmbed).catch(console.error);
+  }
+};
