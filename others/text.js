2f0d614362b4fd2f8e669ee1891aff4b0c33b4e9
/MrRobot <87871568+kakr0b0t@users.noreply.github.com>
2021-08-03 13:31:09 +0300

    Update text.js

--- others/text.js
+++ others/text.js
@@ -10,7 +10,7 @@ module.exports = {
      
     let helpEmbed = new MessageEmbed()
     .setColor('#116d56')
-    .setDescription(`To Invite Rupo To Your Server [click here](https://discord.com/api/oauth2/authorize?client_id=868194752619773962&permissions=8&scope=bot) - Need more help? join Our Server [click here](https://discord.gg/A6R2CJENGn) \n\nThere All Text Commands List \n\n${prefix}textkurdish, ${prefix}textarabic, ${prefix}textenglish, ${prefix}textturkish, ${prefix}textpersian`)
+    .setDescription(`Invite RupoBot To Your Server By [click here](https://discord.com/api/oauth2/authorize?client_id=868194752619773962&permissions=8&scope=bot)\n\nThere All Text Commands List \n\n${prefix}textkurdish, ${prefix}textarabic, ${prefix}textenglish, ${prefix}textturkish, ${prefix}textpersian`)
     .setFooter(`${message.author.tag}`, message.author.avatarURL)
     helpEmbed.setTimestamp();
 
