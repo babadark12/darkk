f92552e76651431894097c6eaee9d58b5ab9cc62
/MrRobot <87871568+kakr0b0t@users.noreply.github.com>
2021-08-03 13:06:31 +0300

    Update help.js

--- others/help.js
+++ others/help.js
@@ -11,15 +11,7 @@ module.exports = {
     let helpEmbed = new MessageEmbed()
     .setColor('#116d56')
     .setTitle("Need Help? Here Are All Commands For Useing")
-    .setDescription(`[Support](https://discord.gg/A6R2CJENGn) - [Invite](https://discord.com/api/oauth2/authorize?client_id=868194752619773962&permissions=8&scope=bot)`)
-    .addField("General Commands", `\`help\`, \`ping\`, \`prefix\`, \`uptime\`, \`avatar\`, \`invites\`, \`suggest\`, \`userinfo\`, \`serverinfo\`, \`github\`, \`support\`, \`botinfo\`, \`weather\`, \`embed\``)
-    .addField("Music Commands", `\`filter\`, \`loop\`, \`lyrics\`, \`np\`, \`pause\`, \`play\`, \`queue\`, \`radio\`, \`remove\`, \`resume\`, \`search\`, \`shuffle\`, \`skip\`, \`skipto\`, \`stop\`, \`volume\``)
-    .addField("Moderation Commands", `\`lock\`, \`unlock\`, \`ban\`, \`unban\`, \`emojiinfo\`, \`se\`, \`bans\`, \`vkick\`, \`clear\`, \`emojilist\`, \`kick\`, \`warn\`, \`cinvite\`, \`channelinfo\`, \`channels\`, \`roleinfo\`, \`lockdown\`, \`tempban\`, , \`tempmute\`, \`serverlock\`, \`serverunlock\`, \`addrole\`, \`rmvrole\``)
-    .addField("Fun Commands", `\`slap\`, \`hug\`, \`howgay\`,‌ \`meme\`, \`cry\`, \`kiss\`, \`clyde\`, \`captcha\`, \`phcomment\`, \`cuddle\`, \`joke\`, \`poke\`, \`baka\`, \`pat\`, \`punch\`, \`spank\`, \`dance\`, \`rate\`, \`advice\`, \`ascii\` \`dicksize\``)
-    .addField("Text Commands", `\`textkurdish\`, \`textarabic\`, \`textenglish\`, \`textturkish\`, \`textpersian\``)
-    .addField("Gif Commands", `\`boy\`, \`girl\`, ‌\`couple\`, \`anime\`, \`emoji\`, \`animal\`, \`neon\``)
-    .addField("Backup Commands", `\`backup-create\`, \`backup-load\`, \`backup-info\``)
-    .addField("Game Commands", `\`rps\`, \`slots\`, \`iq\`, \`hack\`, \`8ball\``)
+    .setDescription(`Invite RupoBot to your server by [click here](https://discord.com/api/oauth2/authorize?client_id=868194752619773962&permissions=8&scope=bot)\n\nTo view each part of the commands \n\n+Other +Gifs +Picture +Fun +Game +Text +Moderation +Backup\nNeed More Help? Join [our server](https://discord.gg/A6R2CJENGn)Our channel on [Youtube]()If you have any quesstion send message for MrRobot#7265`)
     .setFooter(`Requested by ${message.author.tag}`, message.author.displayAvatarURL({dynamic: true}))
     helpEmbed.setTimestamp();
 
