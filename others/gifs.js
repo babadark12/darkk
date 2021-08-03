6132012de84e7ff0d1fa474ffe6745377d4b17cc
/MrRobot <87871568+kakr0b0t@users.noreply.github.com>
2021-08-03 14:10:40 +0300

    Create gifs.js

---
+++ others/gifs.js
@@ -0,0 +1,19 @@
+const { MessageEmbed } = require("discord.js");
+const { lineReply } = require("discord-reply");
+
+module.exports = {
+  name: "Gifs",
+  aliases: ["gifs", "g", "gif"],
+  description: "Display all commands and descriptions",
+  execute(message) {
+    let commands = message.client.commands.array();
+     
+    let helpEmbed = new MessageEmbed()
+    .setColor('#116d56')
+    .setDescription(`Invite RupoBot To Your Server By [click here](https://discord.com/api/oauth2/authorize?client_id=868194752619773962&permissions=8&scope=bot)\n\nThere All Gif Commands List \n\n${prefix}boy, ${prefix}girl, ‌${prefix}couple, ${prefix}anime, ${prefix}emoji, ${prefix}animal, ${prefix}neon`)
+    .setFooter(`${message.author.tag}`, message.author.avatarURL)
+    helpEmbed.setTimestamp();
+
+    message.lineReplyNoMention(helpEmbed).catch(console.error);
+  }
+};
