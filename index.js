726f05ad1a18ba99222073c6de7fc16ebef4a6b7
/MrRobot <87871568+kakr0b0t@users.noreply.github.com>
2021-08-03 15:30:10 +0300

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ client.on(`ready`, () => {
       });
       
 
-    client.user.setActivity(`Bot will update soon`, { type: "COMPETING"});
+    client.user.setActivity(`+help ${client.guilds.cache.size} Guilds`, { type: "COMPETING"});
 
     
    
