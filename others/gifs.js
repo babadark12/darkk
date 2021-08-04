159569e5084698dc253b7da7e3aeab9d6d7852be
/MrRobot <87871568+kakr0b0t@users.noreply.github.com>
2021-08-05 00:19:22 +0300

    Update gifs.js

--- others/gifs.js
+++ others/gifs.js
@@ -10,7 +10,7 @@ module.exports = {
      
     let helpEmbed = new MessageEmbed()
     .setColor('#116d56')
-    .setDescription(`Invite RupoBot To Your Server By [click here](https://discord.com/api/oauth2/authorize?client_id=868194752619773962&permissions=8&scope=bot)\n\nThere All Gif Commands List \n\n${prefix}boy, ${prefix}girl, ‌${prefix}couple, ${prefix}anime, ${prefix}emoji, ${prefix}animal, ${prefix}neon`)
+    .setDescription(`Invite Loser Bot To Your Server By [click here](https://discord.com/api/oauth2/authorize?client_id=868194752619773962&permissions=8&scope=bot)\n\nThere All Gif Commands List \n\n${prefix}boy, ${prefix}girl, ‌${prefix}couple, ${prefix}anime, ${prefix}emoji, ${prefix}animal, ${prefix}neon`)
     .setFooter(`${message.author.tag}`, message.author.avatarURL)
     helpEmbed.setTimestamp();
 
