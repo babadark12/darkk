7f2577c54f2f077deb5966a0288e373cbb81f02b
/MrRobot <87871568+kakr0b0t@users.noreply.github.com>
2021-08-05 10:47:29 +0300

    Update help.js

--- others/help.js
+++ others/help.js
@@ -10,7 +10,7 @@ module.exports = {
      
     let helpEmbed = new MessageEmbed()
     .setColor('#116d56')
-    .setDescription(`Invite Loser Bot to your server by [click here](https://discord.com/api/oauth2/authorize?client_id=868194752619773962&permissions=8&scope=bot)\n\nNeed Help? This is part of the commands \n\n+Info +Gifs +Pic +Fun +Game +Text +Moderation +Backup +Music\n\nNeed More Help? Join [our server](https://discord.gg/A6R2CJENGn) \nOur channel on [Youtube](https://youtube.com/channel/UC_EfAg6_maZeTyQN2T-0pzg) \nIf you have any quesstion send message for MrRobot#7265`)
+    .setDescription(`Invite Loser Bot to your server by [click here](https://discord.com/api/oauth2/authorize?client_id=872588477391331399&permissions=8&scope=bot)\n\nNeed Help? This is part of the commands \n\n+Info +Gifs +Pic +Fun +Game +Text +Moderation +Backup +Music\n\nNeed More Help? Join [our server](https://discord.gg/A6R2CJENGn) \nOur channel on [Youtube](https://youtube.com/channel/UC_EfAg6_maZeTyQN2T-0pzg) \nIf you have any quesstion send message for MrRobot#7265`)
     .setFooter(`Requested by ${message.author.tag}`, message.author.displayAvatarURL({dynamic: true}))
     helpEmbed.setTimestamp();
 
