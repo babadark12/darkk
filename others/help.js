fed72b143c065d8135d6267cf9ef9070cf8989b2
/MrRobot <87871568+kakr0b0t@users.noreply.github.com>
2021-08-10 00:37:14 +0300

    Update help.js

--- others/help.js
+++ others/help.js
@@ -10,7 +10,7 @@ module.exports = {
      
     let helpEmbed = new MessageEmbed()
     .setColor('#116d56')
-    .setDescription(`You Can Add Me By [click here](https://discord.com/api/oauth2/authorize?client_id=872588477391331399&permissions=8&scope=bot)\n\nNeed Help? This is part of the commands \n\n+Info +Gifs +Pic +Fun +Game +Text +Moderation +Backup +Music\n\nNeed More Help? Join [our server](https://discord.gg/A6R2CJENGn) \nOur channel on [Youtube](https://youtube.com/channel/UC_EfAg6_maZeTyQN2T-0pzg) \nIf you have any quesstion send message for Delta#7265`)
+    .setDescription(`You Can Add Me To Your Server By [click here](https://discord.com/api/oauth2/authorize?client_id=872588477391331399&permissions=8&scope=bot)\n\nNeed Help? This is part of the commands \n\n+Info +Gifs +Pic +Fun +Game +Text +Moderation +Backup +Music\n\nNeed More Help? Join [our server](https://discord.gg/A6R2CJENGn) \nOur channel on [Youtube](https://youtube.com/channel/UC_EfAg6_maZeTyQN2T-0pzg) \nQuesstion Or Suggest Dms: Delta#7265`)
     .setFooter(`Requested by ${message.author.tag}`, message.author.displayAvatarURL({dynamic: true}))
     helpEmbed.setTimestamp();
 
