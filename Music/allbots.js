95293ba7b3008d8a9e050419a5f24b313291459c
/MrRobot <87871568+kakr0b0t@users.noreply.github.com>
2021-08-10 00:42:27 +0300

    Create allbots.js

---
+++ Music/allbots.js
@@ -0,0 +1,25 @@
+const { MessageEmbed } = require('discord.js');
+const { lineReply } = require("discord-reply");
+
+module.exports = {
+    name: 'allbots',
+    description: 'show all bots in guild',
+    usage: 'allbots',
+    category: 'info',
+    async execute(message, args, client) {
+    
+    let checked = '🔋';
+    let unchecked = '❌';
+
+
+    const allbots = message.guild.members.cache.filter(m => m.user.bot).map((m) => m).map((m) => `${m.user.flags ? checked : unchecked} ${m.user.tag} (${m.id})`).join('\n');
+
+    const embed = new MessageEmbed()
+    .setColor('#116d56')
+    .setAuthor(message.author.tag, message.author.displayAvatarURL({ dynamic: true }))
+    .setDescription(allbots)
+    .setFooter(client.user.tag, client.user.displayAvatarURL())
+    .setTimestamp();
+    message.lineReplyNoMention(embed)
+  }
+};
