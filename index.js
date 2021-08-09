91d1bf49fe8c86db3b74b46f0252aa83e1c75df0
/MrRobot <87871568+kakr0b0t@users.noreply.github.com>
2021-08-09 15:10:16 +0300

    Update index.js

--- index.js
+++ index.js
@@ -189,6 +189,39 @@ client.on("guildDelete", guild => {
   channel.send(embed);
 });
 
+client.on('clickButton', async (button) => {
+  if (button.id === 'inviteyes') {
+    button.defer()
+    
+    const inviteyb = new discord.MessageEmbed()
+    .setTitle("Thanks!")
+    .setDescription(`Here Is My Invite Links: \nServer Moderator: **[Click Me](https://discord.gg/yRdaYPgW)**
+    Server Helper: **[Click Me](https://discord.gg/5cNZav2xzD)** \n Recommended: **[Click Me](https://discord.com/api/oauth2/authorize?client_id=872588477391331399&permissions=8&scope=bot)**`)
+    .setColor("GREEN");
+
+    const joindsc = new MessageButton()
+    .setStyle('url')
+    .setLabel('Join Our Support Server!')
+    .setURL('https://discord.gg/5cNZav2xzD');
+    button.message.edit({button: joindsc, embed: inviteyb})
+
+  }
+  if(button.id === 'inviteno'){
+    button.defer()
+    const noooyb = new discord.MessageEmbed()
+    .setTitle('Okay Then')
+    .setDescription('But Please Join Our Support Server!')
+    .setColor("RED");
+
+    const joindsc = new MessageButton()
+    .setStyle('url')
+    .setLabel('Join Our Support Server!')
+    .setURL('https://discord.gg/5cNZav2xzD');
+
+    button.message.edit({button: joindsc, embed: noooyb})
+  }
+});
+
 function delay(delayInms) {
  return new Promise(resolve => {
    setTimeout(() => {
