b4fee06baa9a3a0b83d2c8a9d958f88ed9e7ce91
/MrRobot <87871568+kakr0b0t@users.noreply.github.com>
2021-08-09 16:50:17 +0300

    Update index.js

--- index.js
+++ index.js
@@ -195,14 +195,13 @@ client.on("guildDelete", guild => {
 
 client.on("guildCreate", guild => {
     let chx = guild.channels.cache.filter(chx => chx.type === "text").find(x => x.position === 0)
-    const thankEmbed = new MessageEmbed()
+    let embed = new MessageEmbed()
       .setColor('RANDOM')
       .setTitle('Hello!')
       .setDescription(`Woah! Thank You So Much, <@${inviter.id}> For Inviting Me To This Awesome Server (${guild.name})`)
       .setImage('https://cdn.discordapp.com/attachments/811143476522909718/861430392158552094/standard_6.gif')
       .setTimestamp();
-    chx.send(thankEmbed);
-  });
+    chx.send(embed);
 });
 
 function delay(delayInms) {
