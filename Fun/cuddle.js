e9adbfa63367ed5f245c81cdf7d1719b5c4b8fcb
/MrRobot <87871568+kakr0b0t@users.noreply.github.com>
2021-08-10 11:46:49 +0300

    Create cuddle.js

---
+++ Fun/cuddle.js
@@ -0,0 +1,35 @@
+const Discord = require("discord.js");
+const superagent = require("snekfetch");
+const { lineReply } = require("discord-reply");
+
+module.exports = {
+  name: "cuddle",
+  aliases: ["cd"],
+  description: "Cuddle image",
+  usage: "cuddle",
+  async execute(message, args, client) {
+            const user = message.mentions.users.first();
+
+            if(!user)
+                return message.channel.send({embed: {
+                color: 16734039,
+                description: "You must mention someone to cuddle!"
+            }})
+
+            superagent.get('https://nekos.life/api/v2/img/cuddle')
+                .end((err, response) => {
+              const embed = new Discord.MessageEmbed()
+              .setTitle(user.username + " Just got a cuddle from " + message.author.username)
+              .setImage(response.body.url)
+              .setColor(`#116d56`)
+              .setDescription((user.toString() + " got a cuddle from " + message.author.toString()))
+              .setFooter(`this is so cute`)
+              .setURL(response.body.url);
+          message.lineReplyNoMention(embed);
+            }).catch((err) => message.channel.send({embed: {
+                color: 16734039,
+                description: "Something went wrong... :cry:"
+            }}));
+
+        }
+       }
