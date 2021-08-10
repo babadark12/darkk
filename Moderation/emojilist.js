861346c2f881afbda83881072ea9031d07ce1f6a
/MrRobot <87871568+kakr0b0t@users.noreply.github.com>
2021-08-10 22:54:09 +0300

    Update emojilist.js

--- Moderation/emojilist.js
+++ Moderation/emojilist.js
@@ -1,59 +1,25 @@
-const Discord = require("discord.js");
-const { MessageEmbed } = require("discord.js");
+const { RichEmbed } = require("discord.js");
 const { lineReply } = require("discord-reply");
 
 module.exports = {
   name: "emojilist",
-  category: "misc",
-  description: "displays every emoji of the guild, where the command is used",
-  aliases: ["serveremojis", "emojilist"],
-  usage: "hey emojis",
-  botPermission: ["MANAGE_EMOJIS"],
+  category: "<:info:773053660380135424>info",
+  description: "INVITE BOT",
   async execute(message, args, client) {
-    message.delete();
-
+  
     try {
-      let Emojis = "";
-      let EmojisAnimated = "";
-      let EmojiCount = 0;
-      let Animated = 0;
-      let OverallEmojis = 0;
-      function Emoji(id) {
-        return bot.emojis.cache.get(id).toString();
+        let notAnimated = [];
+        let animated = [];
+        message.guild.emojis.cache.forEach(async emoji => {
+          if (emoji.animated) animated.push(emoji.toString());
+          else notAnimated.push(emoji.toString());
+        });
+        if (!animated[0]) animated = ['None'];
+        if (!notAnimated[0]) notAnimated = ['None'];
+        message.lineReplyNoMention('Animated:\n' + animated.join(' ') + '\nNormal:\n' + notAnimated.join(' '), {split:true});
+      } catch (err) {
+        message.lineReplyNoMention('Their was an error!\n' + err).catch();
       }
-
-      message.guild.emojis.cache.forEach(emoji => {
-        OverallEmojis++;
-        if (emoji.animated) {
-          Animated++;
-          EmojisAnimated += Emoji(emoji.id);
-        } else {
-          EmojiCount++;
-          Emojis += Emoji(emoji.id);
-        }
-      });
-
-      let emn = new Discord.MessageEmbed();
-      emn.setTitle(`Server Emojis Of: ${message.guild.name}`);
-      emn.setColor("#116d56");
-      emn.setThumbnail(
-        message.guild.iconURL({ dynamic: true, format: "png", size: 512 })
-      );
-      emn.setDescription(
-        `~~Animated~~ - \`[${Animated}]\`
-
-${EmojisAnimated}
-
-~~Standard~~ - \`[${EmojiCount}]\`
-
-${Emojis}`
-      );
-      message.lineReplyNoMention(emn);
-    } catch (err) {
-      if (err)
-        return message.channel.send(
-          `The Error occuring currently is : ${err.message}`
-        );
-    }
-  }
+      
+}
 };
