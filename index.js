568dffeb0887389e34517131381e0cd0c70041a6
/MrRobot <87871568+kakr0b0t@users.noreply.github.com>
2021-08-10 20:32:42 +0300

    Update index.js

--- index.js
+++ index.js
@@ -84,6 +84,11 @@ for (const file of commandFiles) {
   const command = require(join(__dirname, `Info`, `${file}`));
   client.commands.set(command.name, command);
 }
+commandFiles = readdirSync(join(__dirname, `Text`)).filter((file) => file.endsWith(`.js`));
+for (const file of commandFiles) {
+  const command = require(join(__dirname, `Text`, `${file}`));
+  client.commands.set(command.name, command);
+}
 //COMMANDS //DO NOT TOUCH
 client.on(`message`, async (message) => {
   if (message.author.bot) return;
