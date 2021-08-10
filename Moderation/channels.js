1dd9ca87f7980877adda7b0ef6ce787102126da5
/MrRobot <87871568+kakr0b0t@users.noreply.github.com>
2021-08-10 10:57:36 +0300

    Create channels.js

---
+++ Moderation/channels.js
@@ -0,0 +1,25 @@
+const { MessageEmbed } = require("discord.js");
+const { lineReply } = require("discord-reply");
+
+module.exports = {
+  name: "channels",
+  aliases: ["ch"],
+  description: "Show Gif",
+  usage: "Gif",
+   async execute(message, args, client) {
+        const channels = message.guild.channels.cache;
+        const voiceChannels = channels.filter((channel) => channel.type === "voice").map((channel) => channel.name).join(", ");
+        const textChannels = channels.filter((channel) => channel.type === "text").map((channel) => `<#${channel.id}>`).join(", ");
+
+        const embed = new MessageEmbed()
+            .setColor("#116d56")
+            .setTitle(`${message.guild.name}'s channels`)
+            .addField("**🔊 Voice Channels:**", voiceChannels)
+            .addField("**#️⃣ Text Channels:**", textChannels)
+            .setFooter(message.author.username)
+            .setTimestamp();
+
+
+        message.lineReplyNoMention(embed);
+    }
+};
