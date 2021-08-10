cc1bf78e695f4006293b6a3c69972f8f4e75e7b5
/MrRobot <87871568+kakr0b0t@users.noreply.github.com>
2021-08-10 13:05:42 +0300

    Update invite.js

--- Music/invite.js
+++ Music/invite.js
@@ -1,45 +1,26 @@
-const { Client, Message, MessageEmbed, Discord } = require('discord.js');
-const { MessageButton } = require('discord-buttons');
-
+const discord = require('discord.js'); //Define the discord.js module
+const client = new discord.Client(); //Creating discord.js client (constructor)
+require('discord-buttons')(client);
 module.exports = {
-  name: "invite",
-  aliases: ["invitelink", "vote"],
-  category: "utility",
-  description: "Give You My Invite Link, Etc!",
-  usage: "Invite",
-  guildOnly: true,
-  cooldown: 1,
+  name: "inv",
+  description: "a cool invite bot Commnd ",
+  category: "info",
+ // users will need premium to execute this
   async execute(message, args, client) {
-    const embed = new Discord.MessageEmbed()
-       .setColor("GREEN")
-        .setTitle("🙏Thanks🙏")
-        .addField(
-          "Invite Me",
-         `[Click Me](https://discord.com/api/oauth2/authorize?client_id=849903077690572800&permissions=4232052735&redirect_uri=https%3A%2F%2Fdgh-bot.ddns.net%2Flogin&response_type=code&scope=applications.commands%20bot%20identify%20guilds%20guilds.join)`)
-         .addField(
-          "Vote",
-          `[Click Me](https://discord4bots.ddns.net/bot/849903077690572800/)`
-        )
-        .addField("Support Server", `[Click Me](${Support})`)
-        .addField("Creator", `Delta#7265`)
-        .setImage(
-          `https://discordapp.com/api/guilds/${Server_ID}/embed.png?style=banner3`
-        );
-  
-const yes = new client.button.MessageButton()
-        .setStyle("green")
-        .setLabel("Join Our Support Server!")
-        .setURL("https://discord.gg/ymrrqwZB");
-    
-const web = new client.button.MessageButton()
-        .setStyle("green")
-        .setLabel("Websites")
-        .setURL("https://dgh-bot.ddns.net");
-   
-    client.button.send(null, {
-      channel: message.channel.id,
-      embed: embed,
-      buttons: [[yes, web]]
-    });
-}
-};
+
+        
+     message.buttons('Invite me', {
+            buttons: [
+                {
+                    style: 'green',
+                    label: 'Click to function!',
+                    id: 'click_to_function'
+                },
+                {
+                    style: 'url',
+                    label: 'Vote for me!',
+                    url:'https://dsc.gg/automodbot'
+                }
+            ]
+        })
+  }}
