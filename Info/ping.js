9b6a056c18a382a15624f5076a43494e3a9f40d3
/MrRobot <87871568+kakr0b0t@users.noreply.github.com>
2021-08-10 20:02:10 +0300

    Create ping.js

---
+++ Info/ping.js
@@ -0,0 +1,24 @@
+const Discord = require("discord.js");
+const { MessageEmbed } = require("discord.js");
+const { lineReply } = require("discord-reply");
+
+module.exports = {
+  name: "ping",
+  aliases: [],
+  description: "Pong!",
+  usage: "Ping",
+  async execute(message, args, client) {
+   
+   
+
+    const embed = new MessageEmbed()
+      .setColor("#116d56")
+      .setDescription(`Pong ${client.ws.ping} ms`)
+      .setFooter(`Requested By ${message.author.username}`)
+      .setTimestamp();
+
+    mmssage.lineReplyNoMention(`Pong🏓`, embed);
+
+    
+  }
+};
