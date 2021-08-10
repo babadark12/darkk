8e90613d4a2f381e62035083d02e9a51959607aa
/MrRobot <87871568+kakr0b0t@users.noreply.github.com>
2021-08-10 11:46:07 +0300

    Create cry.js

---
+++ Fun/cry.js
@@ -0,0 +1,22 @@
+const discord = require("discord.js");
+const { Random } = require("something-random-on-discord");
+const random = new Random();
+const { lineReply } = require("discord-reply");
+
+module.exports = {
+  name: "cry",
+  category: "fun",
+  description: "Cry with gif",
+  async execute(message, args, client) {
+    
+    let data = await random.getAnimeImgURL("cry");
+    
+    let embed = new discord.MessageEmbed()
+    .setImage(data)
+    .setColor("#116d56")
+    .setDescription(`Someone Talk With ${message.author.username} they are crying🥺!?`)
+    .setTimestamp()
+    
+    message.lineReplyNoMention(embed);
+  }
+};
