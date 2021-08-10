b1253177aace7f438f6eff65e05bf59ce236f069
/MrRobot <87871568+kakr0b0t@users.noreply.github.com>
2021-08-10 13:14:46 +0300

    Update index.js

--- index.js
+++ index.js
@@ -79,6 +79,21 @@ for (const file of commandFiles) {
   const command = require(join(__dirname, `Fun`, `${file}`));
   client.commands.set(command.name, command);
 }
+for (let file of fs.readdirSync("./events/")) {
+  if (file.endsWith(".js")) {
+    let fileName = file.substring(0, file.length - 3);
+    let fileContents = require(`./events/${file}`);
+    fileContents(client);
+    const description = {
+      name: fileName,
+      filename: file,
+      version: `4.8`
+    };
+    console.log(
+      `⬜️ Module: ${description.name} | Loaded version ${description.version} | form("${description.filename}")`
+    );
+  }
+}
 //COMMANDS //DO NOT TOUCH
 client.on(`message`, async (message) => {
   if (message.author.bot) return;
@@ -176,36 +191,6 @@ message.channel.send(pingedembed);
 
 
 });
-
-const { readdirSync } = require("fs");
-readdirSync("./commands/").forEach(dir => {
-  const commands = readdirSync(`./commands/${dir}/`).filter(file =>
-    file.endsWith(".js")
-  );
-  for (let file of commands) {
-    let command = require(`./commands/${dir}/${file}`);
-    console.log(`${command.name} Has Been Loaded - ✅`);
-    if (command.name) client.commands.set(command.name, command);
-    if (command.aliases) {
-      command.aliases.forEach(alias => client.aliases.set(alias, command.name));
-    }
-  }
-});
-for (let file of fs.readdirSync("./events/")) {
-  if (file.endsWith(".js")) {
-    let fileName = file.substring(0, file.length - 3);
-    let fileContents = require(`./events/${file}`);
-    fileContents(client);
-    const description = {
-      name: fileName,
-      filename: file,
-      version: `4.8`
-    };
-    console.log(
-      `⬜️ Module: ${description.name} | Loaded version ${description.version} | form("${description.filename}")`
-    );
-  }
-}
 
 client.on("guildCreate", guild => {
   let channel = client.channels.cache.get("871701453939560488");
