d0b2b62c1060b3324454dc0f4392f209b5e1c950
/MrRobot <87871568+kakr0b0t@users.noreply.github.com>
2021-08-10 18:20:35 +0300

    Create support.js

---
+++ Info/support.js
@@ -0,0 +1,17 @@
+const Discord = module.require("discord.js");
+const { lineReply } = require("discord-reply");
+
+module.exports = {
+  name: "support",
+  aliases: ["supp"],
+  description: "Show Gif",
+  usage: "Gif",
+  async execute(message, args, client) {
+ 
+await message.lineReplyNoMention({embed: {
+ color: 3447003,
+ description: "https://discord.gg/A6R2CJENGn"
+}});
+
+}
+}
