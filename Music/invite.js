0da8b8e37be17bc271f8dd40d6b4f018a3df1fec
/MrRobot <87871568+kakr0b0t@users.noreply.github.com>
2021-08-10 12:52:27 +0300

    Update invite.js

--- Music/invite.js
+++ Music/invite.js
@@ -1,1 +1,46 @@
+const Discord = require("discord.js");
+const db = require("quick.db");
 
+module.exports = {
+  name: "invite",
+  aliases: ["invitelink", "vote"],
+  category: "utility",
+  description: "Give You My Invite Link, Etc!",
+  usage: "Invite",
+  guildOnly: true,
+  cooldown: 5,
+  async execute(message, args, client) {
+    message.delete();
+    const embed = new Discord.MessageEmbed()
+       .setColor("GREEN")
+        .setTitle("🙏Thanks🙏")
+        .addField(
+          "Invite Me",
+         `[Click Me](https://discord.com/api/oauth2/authorize?client_id=849903077690572800&permissions=4232052735&redirect_uri=https%3A%2F%2Fdgh-bot.ddns.net%2Flogin&response_type=code&scope=applications.commands%20bot%20identify%20guilds%20guilds.join)`)
+         .addField(
+          "Vote",
+          `[Click Me](https://discord4bots.ddns.net/bot/849903077690572800/)`
+        )
+        .addField("Support Server", `[Click Me](${Support})`)
+        .addField("Creator", `Delta#7265`)
+        .setImage(
+          `https://discordapp.com/api/guilds/${Server_ID}/embed.png?style=banner3`
+        );
+  
+const yes = new client.button.MessageButton()
+        .setStyle("green")
+        .setLabel("Join Our Support Server!")
+        .setURL("https://discord.gg/ymrrqwZB");
+    
+const web = new client.button.MessageButton()
+        .setStyle("green")
+        .setLabel("Websites")
+        .setURL("https://dgh-bot.ddns.net");
+   
+    client.button.send(null, {
+      channel: message.channel.id,
+      embed: embed,
+      buttons: [[yes, web]]
+    });
+}
+};
