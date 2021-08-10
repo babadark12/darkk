bbc69f2d4825c6f8503038c06a3c7602c7840a47
/MrRobot <87871568+kakr0b0t@users.noreply.github.com>
2021-08-10 11:47:16 +0300

    Create dance.js

---
+++ Fun/dance.js
@@ -0,0 +1,31 @@
+const discord = require("discord.js");
+const {MessageEmbed} = require ("discord.js");
+const fetch = require ("node-fetch");
+const { lineReply } = require("discord-reply");
+
+module.exports = {
+    name: "dance",
+    aliases:null,
+    description: "Are ya happy? Lets Dance!",
+    usage: 'dance',
+    category: 'emotes',
+
+
+    async execute(message, args, client) {
+
+        const data = await fetch ("https://miss.perssbest.repl.co/api/v2/dance") .then (res => res.json ());
+
+        let embed = new discord.MessageEmbed()
+
+        .setFooter(message.author.username)
+        .setDescription(`${message.author.username} Is Dancing!`)
+        .setImage(`${data.image}`)
+        .setTitle(`♪ \(^ω^\ )`)
+        .setColor("#116d56")
+        .setTimestamp()
+
+      message.lineReplyNoMention(embed);
+
+    }
+
+};
