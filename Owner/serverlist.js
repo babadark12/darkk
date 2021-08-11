c6c4b5996f5997eb7ea397b1946aee94ab2d9f1e
/MrRobot <87871568+kakr0b0t@users.noreply.github.com>
2021-08-11 15:45:11 +0300

    Update serverlist.js

--- Owner/serverlist.js
+++ Owner/serverlist.js
@@ -26,7 +26,7 @@ module.exports = {
                 client.guilds.cache
                     .sort((a, b) => b.memberCount - a.memberCount)
                     .map(r => r)
-                    .map((r, i) => `**${i + 1}** - ${r.name} | ${r.memberCount} Members\nID - ${r.id}\nOwner ${r.owner.user.tag}`)
+                    .map((r, i) => `**${i + 1}** - ${r.name} | ${r.memberCount} Members\nID - ${r.id}`)
                     .slice(0, 10)
                     .join("\n");
 
@@ -72,7 +72,7 @@ module.exports = {
                             .sort((a, b) => b.memberCount - a.memberCount)
                             .map(r => r)
                             .map(
-                                (r, i) => `**${i + 1}** - ${r.name} | ${r.memberCount} Members\nOwner ${r.owner.user.tag}`
+                                (r, i) => `**${i + 1}** - ${r.name} | ${r.memberCount} Members\nID - ${r.id}`
                             )
                             .slice(i0, i1)
                             .join("\n");
@@ -108,7 +108,7 @@ module.exports = {
                             .sort((a, b) => b.memberCount - a.memberCount)
                             .map(r => r)
                             .map(
-                                (r, i) => `**${i + 1}** - ${r.name} | ${r.memberCount} Members\nOwner ${r.owner.user.tag}`
+                                (r, i) => `**${i + 1}** - ${r.name} | ${r.memberCount} Members\nID - ${r.id}`
                             )
                             .slice(i0, i1)
                             .join("\n");
