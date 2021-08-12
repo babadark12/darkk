3e492cbc1da67a5b8f65a6baae9b65593102be35
/MrRobot <87871568+kakr0b0t@users.noreply.github.com>
2021-08-13 00:43:37 +0300

    Update hug.js

--- Fun/hug.js
+++ Fun/hug.js
@@ -16,7 +16,7 @@ module.exports = {
     const embed = new MessageEmbed()
       .setTitle(`${message.author.username} Hugged ${hugged}`)
       .setFooter(message.author.username)
-      .setColor("#116d56")
+      .setColor("#FF0000")
       .setDescription(`[Click here if the image failed to load.](${data.url})`)
       .setImage(`${data.url}`)
       .setTimestamp();
