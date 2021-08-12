41f975a8207e8a932c898e03c39df6aca6d7a01f
/MrRobot <87871568+kakr0b0t@users.noreply.github.com>
2021-08-13 00:43:56 +0300

    Update hwogay.js

--- Fun/hwogay.js
+++ Fun/hwogay.js
@@ -17,7 +17,7 @@ module.exports = {
     let Result = Math.floor(Math.random() * 101);
 
     let embed = new MessageEmbed()
-      .setColor("#116d56")
+      .setColor("#FF0000")
       .setTitle(`Gay Machine`)
       .setDescription(`${Member.user.username} Is ${Result}% Gay 🏳️‍🌈`)
       .setFooter(`Requested by ${message.author.username}`)
