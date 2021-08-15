a7c8902c455efc14049444e6acecb8d105ce2a62
/MrRobot <87871568+kakr0b0t@users.noreply.github.com>
2021-08-15 15:03:50 +0300

    Update invite.js

--- Info/invite.js
+++ Info/invite.js
@@ -1,24 +1,25 @@
 const Discord = require("discord.js");
 const db = require("quick.db");
-const { lineReply } = require("discord-reply");
 
 module.exports = {
-  name: "invite",
-  aliases: ["invitelink"],
-  category: "utility",
-  description: "Give You My Invite Link, Etc!",
-  usage: "Invite",
-  guildOnly: false,
-  cooldown: 5,
-  async execute(message, args, client) {
-    message.delete();
-    const Invite = `https://discord.com/api/oauth2/authorize?client_id=872588477391331399&permissions=8&scope=bot`;
-    const Embed = new Discord.MessageEmbed()
-      .setColor("#FF0000")
-      .setTitle("There All Links")
-      .addField("Invite Me", `[Click Me](${Invite})`)
-      .addField("Support Me", `[Click Me](https://discord.gg/A6R2CJENGn)`)
-      .addField("Developer", `Ds.MrRobot#7265`)
-    return message.lineReplyNoMention(Embed)
-  }
-};
+    name: 'invite',
+    aliases: ['inv'],
+    description: '',
+    usage: '',
+    async execute(message, args, client) {
+      
+        message.channel.send(
+            new MessageEmbed()
+                .setColor('BLUE')
+                .setAuthor(message.author.tag)
+                .setTitle("Invite & Support Link!")
+                .addField("**Invite Link**", `[Click here to invite me](https://discord.com/oauth2/authorize?client_id=${client.user.id}&scope=bot&permissions=8)`)
+                .addField("**Support Server**", `[Click to join support Server](https://discord.gg/sCypg2pj)`)
+                .setFooter(`Requested by ${message.author.tag}`, client.user.displayAvatarURL())
+                .setTimestamp()
+
+
+        )
+
+    }
+}
