d997a8319acc01fb7e188fb21ee34bb3dedfa89d
/MrRobot <87871568+kakr0b0t@users.noreply.github.com>
2021-08-15 21:47:03 +0300

    Update hide.js

--- Moderation/hide.js
+++ Moderation/hide.js
@@ -16,7 +16,7 @@ module.exports = {
 
 if(!message.member.hasPermission("MANAGE_CHANNELS")) return message.lineReplyNoMention(
 new MessageEmbed()
-.setColor("#116d56")
+.setColor("#FF0000")
 .setDescription("**You Need `MANAGE_CHANNELS` Permission To Use This Command!**")
 .setFooter(`${message.author.tag}`, message.author.avatarURL()))
         let channel = message.mentions.channels.first();
