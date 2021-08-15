9d1029fe659aebea6d734d67aa73a4ce2a957a7c
/MrRobot <87871568+kakr0b0t@users.noreply.github.com>
2021-08-15 20:50:51 +0300

    Update help.js

--- others/help.js
+++ others/help.js
@@ -10,8 +10,8 @@ module.exports = {
      
     let helpEmbed = new MessageEmbed()
     .setColor('#FF0000')
-    .addField("General Info", `help, ping, prefix, uptime, avatar, invites, suggest, userinfo, serverinfo, , serverstats, github, support, botinfo, weather, embed, allbots, se, emojiinfo`)
-    .addField("Moderation", `lock, unlock, ban, unban, bans, vkick, purge, emojilist, kick, warn, cinvite, channelinfo, channels, roleinfo, lockdown, tempban, tempmute, serverlock, serverunlock, addrole, rmvrole, mute, unmute, antivc, unantivc`)
+    .addField("General Info", `help, ping, prefix, uptime, avatar, invites, suggest, userinfo, serverinfo, , serverstats, github, support, botinfo, weather, embed, allbots, se, emojiinfo, roles`)
+    .addField("Moderation", `lock, unlock, ban, unban, bans, vkick, purge, emojilist, kick, warn, cinvite, channelinfo, channels, roleinfo, lockdown, tempban, tempmute, serverlock, serverunlock, addrole, rmvrole, mute, unmute, antivc, unantiv`)
     .addField("Funny", `slap, hug, howgay,‌ meme, cry, kiss, clyde, captcha, phcomment, cuddle, joke, poke, baka, pat, punch, spank, dance, rate, advice, ascii, dicksize`)
     .addField("Music", `filter, loop, lyrics, np, pause, play, queue, radio, remove, resume, search, shuffle, skip, skipto, stop, volume`)
     .addField("Text", `textkurdish, textarabic, textenglish, textturkish, textpersian`)
