41d7ff04a0a9a3ca264d4d1b5a83307e71dfe982
/MrRobot <87871568+kakr0b0t@users.noreply.github.com>
2021-08-16 16:37:50 +0300

    Update test.js

--- Info/test.js
+++ Info/test.js
@@ -1,27 +1,120 @@
-
-const { MessageEmbed } = require('discord.js');
+const {MessageEmbed, Client, Message} = require('discord.js')
 
 module.exports = {
-  name: 'test',
-  aliases: [],
-  group: 'utility',
-  desciption: 'Display the larger version of the supplied emoji',
-  parameters: [ 'emoji' ],
-  examples: [
-    'emoji :exampleonly:'
-  ],
- 
-  async execute(message, args, client) {
+    name: 'userinfo',
+    description: 'show all bots in guild',
+    usage: 'allbots',
+    category: 'info',
+    async execute(message, args, client) {
 
-    if (!emoji.match(/<?(a)?:?(\w{2,32}):(\d{17,19})>?/)){
-      return message.channel.send(`\\❌ | ${message.author}, please enter a valid custom emoji!`);
-    };
 
-    return message.channel.send(
-      new MessageEmbed()
-      .setColor('GREY')
-      .setImage('https://cdn.discordapp.com/emojis/' + emoji.match(/\d{17,19}/)[0])
-      .setFooter(`Emoji: ${emoji.match(/\w{2,32}/)[0]} | \©️${new Date().getFullYear()} Mai`)
-    );
-  }
-};
+    if (message.mentions.users.last() /*|| message.mentions.users.last().id === client.user.id*/) {
+        const wuser = message.mentions.users.last();
+        const mUser = message.mentions.members.last();
+        const embed = new MessageEmbed()
+            .setColor(client.color)
+            .setAuthor(wuser.username, wuser.displayAvatarURL({dynamic: true}))
+            .setTitle(`User info for ${wuser.username}`)
+            .addFields(
+                {
+                    name: "User tag",
+                    value: mUser.user.tag,
+                    inline: true
+                },
+                {
+                    name: 'Is bot',
+                    value: mUser.user.bot,
+                    inline: true
+                },
+                {
+                    name: 'ID',
+                    value: message.author.id,
+                    inline: true
+                },
+                {
+                    name: 'Nickname',
+                    value: mUser.nickname || 'None',
+                    inline: true
+                },
+                {
+                    name: 'Joined server',
+                    value: new Date(mUser.joinedTimestamp).toLocaleDateString(),
+                    inline: true
+                },
+                {
+                    name: 'Joined Discord',
+                    value: new Date(wuser.createdTimestamp).toLocaleDateString() + ` (${daysAgo(wuser.createdAt)})`,
+                    inline: true
+                },
+                {
+                    name: 'Role count',
+                    value: mUser.roles.cache.size - 1,
+                    inline: true
+                },
+                {
+                    name: "Roles",
+                    value: mUser.roles.cache.map(role => `<@&${role.id}>`),
+                    inline: true
+                }, {
+                    name: "Status",
+                    value: '`'+wuser.presence.status.toUpperCase() + '`',
+                    inline: true
+                }
+            )
+        message.channel.send(embed)
+    } else {
+
+//        if (message.mentions.users.last().id !== this.client.user.id || message.mentions.users.last().id === this.client.user.id) {
+        const e = new MessageEmbed()
+            .setColor(client.color)
+            .setAuthor(message.author.tag, message.author.displayAvatarURL({dynamic: true}))
+            .setTitle(`User info for ${message.author.username}`)
+            .setThumbnail(message.author.displayAvatarURL({dynamic: true}))
+            .addFields(
+                {
+                    name: 'User tag',
+                    value: message.author.tag,
+                    inline: true
+                }, {
+                    name: 'ID',
+                    value: message.author.id,
+                    inline: true
+                },
+                {
+                    name: 'Is bot',
+                    value: message.author.bot,
+                    inline: true
+                },
+                {
+                    name: "Nickname",
+                    value: message.member.nickname || 'None',
+                    inline: true
+                },
+                {
+                    name: 'Joined server',
+                    value: new Date(message.member.joinedTimestamp).toLocaleDateString(),
+                    inline: true
+                },
+                {
+                    name: "Joined discord",
+                    value: new Date(message.author.createdTimestamp).toLocaleDateString()+ ` (${daysAgo(message.author.createdAt)})`,
+                    inline: true
+                },
+                {
+                    name: "Role count",
+                    value: message.member.roles.cache.size - 1,
+                    inline: true
+                },
+                {
+                    name: "Roles",
+                    value: message.member.roles.cache.map(role => `<@&${role.id}>`),
+                    inline: true
+                }, {
+                    name: "Status",
+                    value: '`'+message.author.presence.status.toUpperCase() + '`',
+                    inline: true
+                }
+            )
+        message.channel.send(e)
+    }
+}
