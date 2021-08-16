9c22c0204ce2fc5a2fe0772384b811392d30ec58
/MrRobot <87871568+kakr0b0t@users.noreply.github.com>
2021-08-16 10:57:20 +0300

    Update invite.js

--- Info/invite.js
+++ Info/invite.js
@@ -8,7 +8,7 @@ module.exports = {
   async execute(message, args, client) {
     const a = new MessageEmbed()
     .setTitle('👋 Hello!')
-    .setDescription('Thank you for checking my links! here is all of my support links!\n\nnmake sure you joined our support server if you want to ask about the repository or something \:>')
+    .setDescription('Thank you for checking my links! here is all of my support links! nmake sure you joined our support server if you want to ask about the repository or something \:>')
     .addField('Adding Me?', '[Click Here](https://discord.com/api/oauth2/authorize?client_id=${client.user.id}&permissions=8&scope=bot)')
     .addField('Support Server?', '[Click Here](https://discord.gg/DZtSGmkf)')
     .addField("Dm Owner For Quoesstion", 'MrRobot#7265')
