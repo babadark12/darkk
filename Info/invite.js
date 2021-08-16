c6263bd27ec536da7d3ba6cbd512298fab1f2a5f
/MrRobot <87871568+kakr0b0t@users.noreply.github.com>
2021-08-16 23:22:19 +0300

    Update invite.js

--- Info/invite.js
+++ Info/invite.js
@@ -9,7 +9,7 @@ module.exports = {
     const a = new MessageEmbed()
     .setTitle('👋 Hello!')
     .setDescription('Thank you for checking my links! here is all of my support links!\nmake sure you joined our support server if you want to ask about the repository or something \:>')
-    .addField('Adding Me?', '[Click Here](https://discord.com/api/oauth2/authorize?client_id=${client.user.id}&permissions=8&scope=bot)')
+    .addField('Adding Me?', '[Click Here](https://discord.com/api/oauth2/authorize?client_id=872588477391331399&permissions=8&scope=bot)')
     .addField('Support Server?', '[Click Here](https://discord.gg/DZtSGmkf)')
     .setThumbnail(client.user.displayAvatarURL({dynamic: true}))
     .setColor('#FF0000')
