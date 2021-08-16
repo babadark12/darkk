fb2cfadff5ad339cc5ba441baf71376e07ad3545
/MrRobot <87871568+kakr0b0t@users.noreply.github.com>
2021-08-16 14:01:10 +0300

    Update userinfo.js

--- Info/userinfo.js
+++ Info/userinfo.js
@@ -1,61 +1,46 @@
-const { jpegVersion } = require("canvas");
-const Discord = require("discord.js"), moment = require("moment"); //npm i discord.js moment
-const Color = "#FF0000";
+const { MessageEmbed } = require("discord.js");
 const { lineReply } = require("discord-reply");
 
 module.exports = {
-  name: "userinfo",
-  aliases: ["userinformation", "whois", 'usi'],
-  category: "info",
-  description: "Shows member information; includes presence.",
-  usage: "Userinfo <user>",
-  async execute(message, args, client) {
+        name: "whois",
+        category: "info",
+        aliases: ["who", "user", "userinfo"],
+        description: "Returns user information",
+        usage: "[name | nickname | mention | ID] (optional)",
+        accessableby: "everyone",
+    async execute(message, args, client) {
+        let member = await message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(r => r.user.username.toLowerCase() === args.join(' ').toLocaleLowerCase()) || message.guild.members.cache.find(r => r.displayName.toLowerCase() === args.join(' ').toLocaleLowerCase()) || message.member;
+      
+        if(!member)
+        return message.channel.send("**Enter A Valid User!**");
+      
+        const joined = formatDate(member.joinedAt);
+        const roles = member.roles.cache
+            .filter(r => r.id !== message.guild.id)
+            .map(r => r.name).join(", ") || 'none';
+        const created = formatDate(member.user.createdAt);
 
-    const User = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.member;
-    const Dism = User.user.discriminator, Bot = User.user.bot ? "Yes" : "No";
-    const Roles = (User.roles.cache.size - 1) == 0 ? "None" : User.roles.cache.size - 1, Avatar = User.user.avatarURL({ dynamic: true });
-    const Badges = await (await User.user.fetchFlags(true)).toArray().map(F => F[0] + (F.slice(1).toLowerCase().replace(/_/g, " "))).join(", "), Permissions = await User.permissions.toArray().map(P => (P[0] + P.slice(1).toLowerCase()).replace(/_/g, " ")).sort().join(", ");
-    const Position = User.id == message.guild.ownerID ? "Owner" : User.hasPermission("ADMINISTRATOR") ? "Administrator" : User.hasPermission("KICK_MEMBERS") ? "Moderator" : User.hasPermission("MANAGE_MESSAGES") ? "Trainee Moderator" : "Member", Status = User.presence.status.charAt(0).toUpperCase() + User.presence.status.slice(1);
-    const Created = await Format(User.user.createdTimestamp), Joined = await Format(User.joinedTimestamp);
-    const Place = User.user.presence.clientStatus ? Object.keys(User.user.presence.clientStatus).map(E => E.charAt(0).toUpperCase() + E.slice(1)) : [], Activity = User.presence.activities ? await Activities(User.presence.activities) : [];
+        const embed = new MessageEmbed()
+            .setTitle("User Inforimation")
+            .setFooter(message.guild.name, message.guild.iconURL())
+            .setThumbnail(member.user.displayAvatarURL({ dynamic: true}))
+            .setColor("#FF0000")
+            .addField("**User information**", `${member.displayName}`)
+            .addField("**ID**", `${member.user.id}`)
+            .addField("**Username**",`${member.user.username}`)
+            .addField("**Tag**", `${member.user.tag}`)
+            .addField("Avatar", `[Click Here](member.user.displayAvatarURL({ dynamic: true})`)
+            .addField("**Created at**", `${created}`)
+            .addField("**Joined at**", `${joined}`)
+            .addField("**Roles**", `${roles}`, true)
+            .setTimestamp()
 
-    const Embed = new Discord.MessageEmbed()
-    .setColor(Color)
-    .setAuthor(message.guild.ownerID == User.user.id ? "Owner" : "User", Avatar)
-    .addField("❯ User:", `${User.user.username}#${Dism}`) //1
-    .addField("❯ ID:", `${User.id}`) //2
-    .addField("❯ Bot:", `${Bot}`) //3
-    .addField("❯ Position:", `${Position}`) //4
-    .addField("❯ Manageable:", `${User.manageable ? "Yes" : "No"}`) //5
-    .addField("❯ Number of Roles:", `${Roles}`) //6
-    .addField("❯ Created:", `${Created}`) // 7
-    .addField("❯ Joined:", `${Joined}`) // 8
+            member.presence.activities.forEach((activity) => {
+        if (activity.type === 'PLAYING') {
+            embed.addField('Currently playing',`\n**${activity.name}**`)
+        }
+            })
 
-    .addField("❯ Avatar URLs:", `[webp](${User.user.displayAvatarURL({ dynamic: true, format: 'webp'})}) | [png](${User.user.displayAvatarURL({ dynamic: true, format: 'png'})}) | [jpg](${User.user.displayAvatarURL({ dynamic: true, format: 'jpeg'})}) | [jpeg](${User.user.displayAvatarURL({ dynamic: true, format: 'jpeg'})}) | [gif](${User.user.displayAvatarURL({ dynamic: true, format: 'gif'})})`) //10
-    .addField("❯ Using Discord On:", `${Place ? Place.join("\n") == "" ? "Unknown" : Place.join("\n") : "None"}`) //11
-    .addField("❯ Activities:", `${Activity.length > 1 ? "" : "Activity"}\n${Activity ? Activity.join("\n") == "" ? "None" : Activity.join("\n") : "None"}`) //11
-    .addField("❯ Badges:", `\n${Badges ? Badges : "No badges"}`) //12
-    .setThumbnail(User.user.displayAvatarURL({ size: 512, dynamic: true }))
-    .setFooter(`Requested by ${message.author.tag}`, message.author.displayAvatarURL({ dynamic: true }))
-    .setTimestamp();
-  return message.lineReplyNoMention(Embed);
-
-    async function Activities(Arr) {
-      const Types = {
-        PLAYING: "Playing",
-        STREAMING: "Streaming",
-        LISTENING: "Listening",
-        WATCHING: "Watching",
-        CUSTOM_STATUS: "Custom Status",
-        COMPETING: "Competing"
-      };
-
-      Arr = Arr.map(E => Types[E.type] + `: ${E.type == "CUSTOM_STATUS" ? `${E.emoji ? E.emoji.name + " " : ""}${E.state}` : E.name}`);
-      return Arr;
-    };
-
-    async function Format(Stamp) {
-       return `${String(moment(Stamp).format("LL")).replace(",", "")} ${moment(Stamp).format("LT")} (${moment(Stamp).fromNow()})`
-    };
-  }
-};
+        message.lineReplyNoMention(embed);
+    }
+}
