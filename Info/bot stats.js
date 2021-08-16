5cfd945cc9f06925353dedc0b3b3659d8001c3c0
/MrRobot <87871568+kakr0b0t@users.noreply.github.com>
2021-08-16 14:19:58 +0300

    Create bot stats.js

---
+++ Info/bot stats.js
@@ -0,0 +1,31 @@
+const { MessageEmbed } = require('discord.js');
+const { version } = require('@root/package.json');
+const ms = require('ms');
+const { lineReply } = require("discord-reply");
+
+module.exports = {
+       name: "serverinfo",
+        description: "Pulls the serverinfo of the guild!",
+        usage: " ",
+        category: "info",
+        accessableby: "everyone",
+        aliases: ["sinfo"],
+    async execute(message, args, client) {
+        const embed = new MessageEmbed()
+            .setThumbnail(client.user.displayAvatarURL())
+            .setColor(message.guild.me.displayHexColor || 'BLUE')
+            .addField('General', [
+                `**❯ Client:** ${client.user.tag} (${client.user.id})`,
+                `**❯ Servers:** ${client.guilds.cache.size.toLocaleString()} `,
+                `**❯ Users:** ${client.guilds.cache.reduce((a, b) => a + b.memberCount, 0).toLocaleString()}`,
+                `**❯ Channels:** ${client.channels.cache.size.toLocaleString()}`,
+                `**❯ Creation Date:** ${client.user.createdAt}`,
+                `**❯ Version:** v1.0.0`,
+                '\u200b'
+            ])
+            .setFooter("Bot Made By MrRobot#7265")
+            .setTimestamp();
+
+        message.lineReplyNoMention(embed);
+    },
+}
