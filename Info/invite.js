8fa715c9685d06693c79025b4121499d3e3feb44
MR.Z <71737369+ZEROO202@users.noreply.github.com>
2021-08-17 14:12:11 +0300

    Update invite.js

--- Info/invite.js
+++ Info/invite.js
@@ -9,11 +9,11 @@ module.exports = {
     const a = new MessageEmbed()
     .setTitle('👋 Hello!')
     .setDescription('Thank you for checking my links! here is all of my support links!\nmake sure you joined our support server if you want to ask about the repository or something \:>')
-    .addField('Adding Me?', '[Click Here](https://discord.com/api/oauth2/authorize?client_id=872588477391331399&permissions=8&scope=bot)')
-    .addField('Support Server?', '[Click Here](https://discord.gg/DZtSGmkf)')
+    .addField('Adding Me?', '[Click Here](https://discord.com/api/oauth2/authorize?client_id=797600275904200715&permissions=8&scope=bot)')
+    .addField('Support Server?', '[Click Here](https://discord.gg/gGhs245N27)')
     .setThumbnail(client.user.displayAvatarURL({dynamic: true}))
     .setColor('#FF0000')
-    .setFooter('Pretty Links');
+    .setFooter('One bot Links');
     return message.channel.send(a)
   }
 }
