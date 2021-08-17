2018f1df2b543fbe72d776af99abb4e4ade7d265
MR.Z <71737369+ZEROO202@users.noreply.github.com>
2021-08-17 11:04:25 +0300

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ client.on(`ready`, () => {
       });
       
 
-    client.user.setActivity(`+help`, { type: "COMPETING"});
+    client.user.setActivity(`>help`, { type: "COMPETING"});
 
     
    
