07e50f70ae12094aec2d60344aadc3d055f530ef
MR.Z <71737369+ZEROO202@users.noreply.github.com>
2021-09-09 00:16:19 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const db = require('quick.db');
 const { TOKEN, PREFIX, AVATARURL, BOTNAME, } = require(`./config.json`);
 const figlet = require("figlet");
 const client = new Client({ disableMentions: `` , partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
-client.login('');
+client.login('ODU4MTI5MTY2ODY4NTQ1NTQ2.YNZpEA.rvfYJmivSvEyNMrHsumlNWB9svk');
 client.commands = new Collection();
 client.setMaxListeners(0);
 client.prefix = PREFIX;
