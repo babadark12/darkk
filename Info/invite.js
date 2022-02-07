5c284c72cc2d0d275fde43e42f32a65fcc3eab8c
mryAshex <69230403+YaSHEX11@users.noreply.github.com>
2022-02-07 07:55:49 +0300

    Update invite.js

--- Info/invite.js
+++ Info/invite.js
@@ -9,8 +9,8 @@ module.exports = {
     const a = new MessageEmbed()
     .setTitle('👋 Hello!')
     .setDescription('Thank you for checking my links! here is all of my support links!\nmake sure you joined our support server if you want to ask about the repository or something \:>')
-    .addField('Adding Me?', '[Click Here](https://discord.com/api/oauth2/authorize?client_id=797600275904200715&permissions=8&scope=bot)')
-    .addField('Support Server?', '[Click Here](https://discord.gg/gGhs245N27)')
+    .addField('Adding Me?', '[Click Here](..)')
+    .addField('Support Server?', '[Click Here](https://discord.gg/8hhVuG58tm)')
     .setThumbnail(client.user.displayAvatarURL({dynamic: true}))
     .setColor('#FF0000')
     .setFooter('One bot Links');
