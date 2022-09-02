7e40f4caa7117dee6fd11c1eef041f447bdaf4c6
babadark12 <112591095+babadark12@users.noreply.github.com>
2022-09-02 05:32:15 +0300

    Update invite.js

--- Info/invite.js
+++ Info/invite.js
@@ -9,8 +9,8 @@ module.exports = {
     const a = new MessageEmbed()
     .setTitle('👋 Hello!')
     .setDescription('Thank you for checking my links! here is all of my support links!\nmake sure you joined our support server if you want to ask about the repository or something \:>')
-    .addField('Adding Me?', '[Click Here](https://discord.com/api/oauth2/authorize?client_id=876234155686514718&permissions=8&scope=bot)')
-    .addField('Support Server?', '[Click Here](https://discord.gg/tpHApgUsyu)')
+    .addField('Adding Me?', '[Click Here](https://discord.com/api/oauth2/authorize?client_id=851307156690698280&permissions=8&scope=bot)')
+    .addField('Support Server?', '[Click Here](https://discord.gg/3D55CHur7D)')
     .setThumbnail(client.user.displayAvatarURL({dynamic: true}))
     .setColor('#FF0000')
     .setFooter('One bot Links');
