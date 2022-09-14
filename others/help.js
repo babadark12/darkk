8d124431a9ac88e9cd25183f56f2daf29eebd2f1
Torpedo <112591095+babadark12@users.noreply.github.com>
2022-09-14 19:49:37 +0300

    Update help.js

--- others/help.js
+++ others/help.js
@@ -25,7 +25,7 @@ const main = new Discord.MessageEmbed()
 •━━━━━━ ✽ • ✽ ━━━━━━•
 7:  \`Copy Server\`
 8:  \`Photo\`
-9: <a:emoji_136:1019198238882009168> \`Game\` <a:emoji_136:1019198238882009168>
+9: <a:emoji_52:1017881674484297778> \`Game\` <a:emoji_52:1017881674484297778>
 **[  SUPPORT  ](https://discord.gg/3D55CHur7D)** - [INVITE](https://discord.com/api/oauth2/authorize?client_id=851307156690698280&permissions=8&scope=bot)
       `)
         .setTimestamp()
