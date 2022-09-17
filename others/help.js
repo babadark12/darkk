9a786a1cdbdc5f7a5c7d2376403c6238350e3cbf
Torpedo <112591095+babadark12@users.noreply.github.com>
2022-09-17 21:57:48 +0300

    Update help.js

--- others/help.js
+++ others/help.js
@@ -172,7 +172,7 @@ const game = new Discord.MessageEmbed()
                 game
         ]
 
-        const emojiList = ["<a:image303:938474925931515944>", "<a:image302:938474927051382834>"];
+        const emojiList = ["⬅️", "➡️"];
 
         const timeout = '120000';
 
